414f082721e81eb84a380a223fde321f840842ad
gubatron <gubatron@gmail.com>
2015-11-04 15:07:54 -0500

    [desktop] moved run.bat to parent directory.

---
+++ desktop/run.bat
@@ -0,0 +1,8 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET LIBPATH=lib\native
+SET PATH=%PATH%;%LIBPATH%
+SET CLASSPATH=..\build\libs\frostwire.jar
+
+REM -Dsun.java2d.opengl=true
+java -Xms32m -Xmx512m -Ddebug=1 -Dcom.sun.management.jmxremote.port=9595 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=127.0.0.1 -Djava.library.path=%LIBPATH% com.limegroup.gnutella.gui.Main
