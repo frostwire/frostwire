400fe282c4c75e6c69a44e907f5b3752e6aec42c
gubatron <gubatron@gmail.com>
2015-12-06 20:42:50 -0500

    [desktop] version bump 6.1.7 build 169 - changelog.

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -39,12 +39,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.1.7";
+    private static final String FROSTWIRE_VERSION = "6.1.8";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 168;
+    private static final int BUILD_NUMBER = 169;
 
     /**
      * Make sure the constructor can never be called.
