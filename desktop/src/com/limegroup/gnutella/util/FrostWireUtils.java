4230444ae3d1b3d3bef85471d62ff81aa426be78
gubatron <gubatron@gmail.com>
2015-12-19 16:53:11 -0500

    [desktop] 6.1.9 build 171. changelog.

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -39,12 +39,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.1.8";
+    private static final String FROSTWIRE_VERSION = "6.1.9";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 170;
+    private static final int BUILD_NUMBER = 171;
 
     /**
      * Make sure the constructor can never be called.
