f715c1e819f92df59aa3c0824ff9bfc9145066c3
Alden Torres <aldenml@gmail.com>
2016-01-05 21:39:08 -0500

    [common] Fixed YT first level regex.

--- common/src/main/java/com/frostwire/search/youtube/YouTubeSearchPerformer.java
+++ common/src/main/java/com/frostwire/search/youtube/YouTubeSearchPerformer.java
@@ -38,7 +38,7 @@ import static com.frostwire.search.youtube.YouTubeUtils.isDash;
  */
 public class YouTubeSearchPerformer extends CrawlRegexSearchPerformer<YouTubeSearchResult> {
 
-    private static final String REGEX = "(?is)<h3 class=\"yt-lockup-title[ ]*\"><a href=\"(?<link>.*?)\".*? title=\"(?<title>.*?)\".*? Duration: (?<duration>.*?)\\.</span>.*?by <a href=\"/user/(?<user>.*?)\"";
+    private static final String REGEX = "(?is)<h3 class=\"yt-lockup-title[ ]*\"><a href=\"(?<link>.*?)\".*? title=\"(?<title>.*?)\".*? Duration: (?<duration>.*?)\\.</span>.*?(by |byline\">)<a href=\"/user/(?<user>.*?)\"";
     private static final Pattern PATTERN = Pattern.compile(REGEX);
 
     private static final int MAX_RESULTS = 15;
