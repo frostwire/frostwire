557b8f0e9928d98b76b4e65dcbad7e367a2579af
Alden Torres <aldenml@gmail.com>
2016-01-26 16:09:34 -0500

    [android] Taking care if save path ends with "FrostWire".

--- android/src/com/frostwire/android/AndroidPaths.java
+++ android/src/com/frostwire/android/AndroidPaths.java
@@ -71,6 +71,10 @@ final class AndroidPaths implements SystemPaths {
 
     private static File storage() {
         String path = ConfigurationManager.instance().getString(Constants.PREF_KEY_STORAGE_PATH);
-        return new File(path, STORAGE_PATH);
+        if (path.endsWith("/" + STORAGE_PATH)) {
+            return new File(path);
+        } else {
+            return new File(path, STORAGE_PATH);
+        }
     }
 }
