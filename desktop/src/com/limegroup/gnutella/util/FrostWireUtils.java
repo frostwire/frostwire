8cc4b6156d25b80903c4c80db27405c2d23a3da9
gubatron <gubatron@gmail.com>
2016-03-31 12:07:15 -0400

    [desktop] changelog 6.2.3 build 178

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -44,7 +44,7 @@ public final class FrostWireUtils {
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 177;
+    private static final int BUILD_NUMBER = 178;
 
     /**
      * Make sure the constructor can never be called.
