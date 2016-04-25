0ae39fc5a78ea97e2bfd9ca1ac97023d7b9dc66f
Marcelina Knitter <knitter.marcelina@gmail.com>
2016-04-25 12:56:00 -0400

    [android] updated my files delete dialog

--- android/src/com/frostwire/android/gui/adapters/menu/DeleteFileMenuAction.java
+++ android/src/com/frostwire/android/gui/adapters/menu/DeleteFileMenuAction.java
@@ -18,15 +18,18 @@
 
 package com.frostwire.android.gui.adapters.menu;
 
+import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.AsyncTask;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.frostwire.android.R;
 import com.frostwire.android.core.FileDescriptor;
 import com.frostwire.android.gui.Librarian;
 import com.frostwire.android.gui.adapters.FileListAdapter;
-import com.frostwire.android.gui.util.UIUtils;
 import com.frostwire.android.gui.views.MenuAction;
 
 import java.util.ArrayList;
@@ -35,7 +38,7 @@ import java.util.List;
 /**
  * @author gubatron
  * @author aldenml
- *
+ * @author marcelinkaaa
  */
 public class DeleteFileMenuAction extends MenuAction {
 
@@ -50,12 +53,44 @@ public class DeleteFileMenuAction extends MenuAction {
 
     @Override
     protected void onClick(Context context) {
-        UIUtils.showYesNoDialog(context, R.string.are_you_sure_delete_files, R.string.application_label, new OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                deleteFiles();
+        showDeleteFilesDialog();
+    }
+
+    private void showDeleteFilesDialog() {
+
+        final Dialog newDeleteFilesDialog = new Dialog(getContext(), R.style.DefaultDialogTheme);
+
+        newDeleteFilesDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        newDeleteFilesDialog.setContentView(R.layout.dialog_default);
+
+        TextView title = (TextView) newDeleteFilesDialog.findViewById(R.id.dialog_default_title);
+        title.setText(R.string.delete_files);
+
+        TextView text = (TextView) newDeleteFilesDialog.findViewById(R.id.dialog_default_text);
+        text.setText(R.string.are_you_sure_delete_files);
+
+        Button buttonNo = (Button) newDeleteFilesDialog.findViewById(R.id.dialog_default_button_no);
+        buttonNo.setText(R.string.cancel);
+        buttonNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newDeleteFilesDialog.dismiss();
             }
         });
+
+        Button buttonYes = (Button) newDeleteFilesDialog.findViewById(R.id.dialog_default_button_yes);
+        buttonYes.setText(android.R.string.ok);
+        buttonYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteFiles();
+                newDeleteFilesDialog.dismiss();
+            }
+        });
+
+        newDeleteFilesDialog.show();
     }
+
 
     private void deleteFiles() {
         int size = files.size();
