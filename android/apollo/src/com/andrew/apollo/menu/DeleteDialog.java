cae00a1b4b193dde593cd71ddcdb3f12ae0f65dd
Marcelina Knitter <knitter.marcelina@gmail.com>
2016-04-25 17:58:21 -0400

    [android] updated My Music delete dialog

--- android/apollo/src/com/andrew/apollo/menu/DeleteDialog.java
+++ android/apollo/src/com/andrew/apollo/menu/DeleteDialog.java
@@ -13,10 +13,13 @@ package com.andrew.apollo.menu;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.andrew.apollo.Config;
 import com.andrew.apollo.cache.ImageFetcher;
 import com.andrew.apollo.utils.ApolloUtils;
@@ -25,9 +28,9 @@ import com.frostwire.android.R;
 
 /**
  * Alert dialog used to delete tracks.
- * <p>
+ * <p/>
  * TODO: Remove albums from the recents list upon deletion.
- * 
+ *
  * @author Andrew Neal (andrewdneal@gmail.com)
  */
 public class DeleteDialog extends DialogFragment {
@@ -48,18 +51,13 @@ public class DeleteDialog extends DialogFragment {
 
     private DeleteDialogCallback onDeleteCallback;
 
-    /**
-     * Empty constructor as per the {@link Fragment} documentation
-     * @param song
-     * @param songAdapter
-     */
     public DeleteDialog() {
     }
 
     /**
      * @param title The title of the artist, album, or song to delete
      * @param items The item(s) to delete
-     * @param key The key used to remove items from the cache.
+     * @param key   The key used to remove items from the cache.
      * @return A new instance of the dialog
      */
     public static DeleteDialog newInstance(final String title, final long[] items, final String key) {
@@ -82,47 +80,57 @@ public class DeleteDialog extends DialogFragment {
      */
     @Override
     public Dialog onCreateDialog(final Bundle savedInstanceState) {
-        final String delete = getString(R.string.context_menu_delete);
+
         final Bundle arguments = getArguments();
         // Get the image cache key
         final String key = arguments.getString("cachekey");
         // Get the track(s) to delete
         mItemList = arguments.getLongArray("items");
-        // Get the dialog title
-        final String title = arguments.getString(Config.NAME);
-        final String dialogTitle = getString(R.string.delete_dialog_title, title);
-        // Initialize the image cache
+        //   Initialize the image cache
         mFetcher = ApolloUtils.getImageFetcher(getActivity());
-        // Build the dialog
-        return new AlertDialog.Builder(getActivity()).setTitle(dialogTitle)
-                .setMessage(R.string.cannot_be_undone)
-                .setPositiveButton(delete, new OnClickListener() {
 
-                    @Override
-                    public void onClick(final DialogInterface dialog, final int which) {
-                        // Remove the items from the image cache
-                        mFetcher.removeFromCache(key);
-                        // Delete the selected item(s)
-                        MusicUtils.deleteTracks(getActivity(), mItemList);
+        final AlertDialog.Builder apolloDeleteFilesDialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        final View inflator = inflater.inflate(R.layout.dialog_default, null);
+        apolloDeleteFilesDialog.setView(inflator);
 
-                        if (getActivity() instanceof DeleteDialogCallback) {
-                            ((DeleteDialogCallback) getActivity()).onDelete(mItemList);
-                        }
+        final TextView dialogTitle = (TextView) inflator.findViewById(R.id.dialog_default_title);
+        dialogTitle.setText(R.string.delete_files_title);
 
-                        if (onDeleteCallback != null) {
-                            onDeleteCallback.onDelete(mItemList);
-                        }
+        TextView text = (TextView) inflator.findViewById(R.id.dialog_default_text);
+        text.setText(R.string.are_you_sure_delete_files_text);
 
-                        dialog.dismiss();
-                    }
-                }).setNegativeButton(R.string.cancel, new OnClickListener() {
+        Button buttonNo = (Button) inflator.findViewById(R.id.dialog_default_button_no);
+        buttonNo.setText(R.string.cancel);
+        buttonNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
 
-                    @Override
-                    public void onClick(final DialogInterface dialog, final int which) {
-                        dialog.dismiss();
-                    }
-                }).create();
+        Button buttonYes = (Button) inflator.findViewById(R.id.dialog_default_button_yes);
+        buttonYes.setText(R.string.delete);
+        buttonYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Remove the items from the image cache
+                mFetcher.removeFromCache(key);
+                // Delete the selected item(s)
+                MusicUtils.deleteTracks(getActivity(), mItemList);
+
+                if (getActivity() instanceof DeleteDialogCallback) {
+                    ((DeleteDialogCallback) getActivity()).onDelete(mItemList);
+                }
+
+                if (onDeleteCallback != null) {
+                    onDeleteCallback.onDelete(mItemList);
+                }
+
+                dismiss();
+            }
+        });
+
+        return apolloDeleteFilesDialog.create();
     }
-
-
 }
