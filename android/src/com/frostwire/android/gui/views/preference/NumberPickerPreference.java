c02769bfc3eed009f366240212a19212c0fd1475
Marcelina Knitter <knitter.marcelina@gmail.com>
2016-05-04 15:39:55 -0400

    [android] WIP on a NumberPickerPreference

--- android/src/com/frostwire/android/gui/views/preference/NumberPickerPreference.java
+++ android/src/com/frostwire/android/gui/views/preference/NumberPickerPreference.java
@@ -16,15 +16,25 @@
  */
 package com.frostwire.android.gui.views.preference;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.preference.DialogPreference;
+import android.preference.Preference;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
+import com.andrew.apollo.utils.PreferenceUtils;
 import com.frostwire.android.R;
+
+import java.util.prefs.Preferences;
 
 public class NumberPickerPreference extends DialogPreference {
     private NumberPicker mPicker;
@@ -47,6 +57,9 @@ public class NumberPickerPreference extends DialogPreference {
         arr.recycle();
 
         setDialogLayoutResource(R.layout.dialog_preference_number_picker);
+
+        //TODO: set the title on the custom dialogTitle TextView below
+        setDialogTitle(null);
     }
 
     public NumberPickerPreference(Context context, AttributeSet attrs) {
@@ -60,28 +73,42 @@ public class NumberPickerPreference extends DialogPreference {
     @Override
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
-        mPicker = (NumberPicker) view.findViewById(R.id.pref_num_picker);
+
+        mPicker = (NumberPicker) view.findViewById(R.id.dialog_preference_number_picker);
         setRange(mStartRange, mEndRange);
         mPicker.setValue((int) getValue());
+
+        //TODO: set correct title
+        TextView dialogTitle = (TextView) view.findViewById(R.id.dialog_preference_number_title);
+
+        //TODO: Connect the positive and negative custom buttons
+        Button noButton = (Button) view.findViewById(R.id.dialog_preference_number_button_no);
+        noButton.setText(R.string.cancel);
+
+        Button yesButton = (Button) view.findViewById(R.id.dialog_preference_number_button_yes);
+        yesButton.setText(android.R.string.ok);
+//        yesButton.setOnClickListener(new PositiveButtonOnClickListener(this));
+
     }
 
-    public void onClick(DialogInterface dialog, int which) {
-        switch (which) {
-        case DialogInterface.BUTTON_POSITIVE:
-            saveValue(mPicker.getValue());
-            final OnPreferenceChangeListener onPreferenceChangeListener = getOnPreferenceChangeListener();
-            if (onPreferenceChangeListener != null) {
-                try {
-                    onPreferenceChangeListener.onPreferenceChange(this, mPicker.getValue());
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                }
-            }
-            break;
-        default:
-            break;
-        }
-    }
+
+//    public void onClick(DialogInterface dialog, int which) {
+//        switch (which) {
+//        case DialogInterface.BUTTON_POSITIVE:
+//            saveValue(mPicker.getValue());
+//            final OnPreferenceChangeListener onPreferenceChangeListener = getOnPreferenceChangeListener();
+//            if (onPreferenceChangeListener != null) {
+//                try {
+//                    onPreferenceChangeListener.onPreferenceChange(this, mPicker.getValue());
+//                } catch (Throwable t) {
+//                    t.printStackTrace();
+//                }
+//            }
+//            break;
+//        default:
+//            break;
+//        }
+//    }
 
     public void setRange(int start, int end) {
         mPicker.setMinValue(start);
@@ -96,4 +123,40 @@ public class NumberPickerPreference extends DialogPreference {
     private long getValue() {
         return getSharedPreferences().getLong(getKey(), mDefault);
     }
+
+
+    //TODO: Connect the positive and negative custom buttons
+//    private class PositiveButtonOnClickListener implements View.OnClickListener {
+//        private final NumberPickerPreference numberPickerPreference;
+//
+//        public PositiveButtonOnClickListener(NumberPickerPreference numberPickerPreference) {
+//            this.numberPickerPreference = numberPickerPreference;
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+//            saveValue(mPicker.getValue());
+//            final OnPreferenceChangeListener onPreferenceChangeListener = getOnPreferenceChangeListener();
+//            if (onPreferenceChangeListener != null) {
+//                try {
+//                    onPreferenceChangeListener.onPreferenceChange(this, mPicker.getValue());
+//                } catch (Throwable t) {
+//                    t.printStackTrace();
+//                }
+//            }
+//        }
+//
+//    }
 }
+
+
+//    private class NegativeButtonOnClickListener implements View.OnClickListener {
+//        public NegativeButtonOnClickListener(NumberPickerPreference numberPickerPreference) {
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+//
+//        }
+//    }
+//}
