f67adc34ed1e317eb745ef3536141ea27d32d135
Alden Torres <aldenml@gmail.com>
2016-05-11 11:00:45 -0400

    [common] Synchronous stop of BTEngine (using destructor of native session)

--- common/src/main/java/com/frostwire/bittorrent/BTEngine.java
+++ common/src/main/java/com/frostwire/bittorrent/BTEngine.java
@@ -30,6 +30,7 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.lang.reflect.Field;
 import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -188,13 +189,24 @@ public final class BTEngine {
 
             downloader = null;
 
-            session.abort();
+            stopRunning();
+            session.swig().delete();
             session = null;
 
             fireStopped();
 
         } finally {
             sync.unlock();
+        }
+    }
+
+    private void stopRunning() {
+        try {
+            Field f = session.getClass().getDeclaredField("running");
+            f.setAccessible(true);
+            f.set(session, false);
+        } catch (Throwable e) {
+            LOG.error("Error setting internal running field to false", e);
         }
     }
 
