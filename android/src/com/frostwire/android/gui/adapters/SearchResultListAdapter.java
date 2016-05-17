8e85517d0097552c76d9169cd930cb9ed1f45bf6
Marcelina Knitter <knitter.marcelina@gmail.com>
2016-05-17 14:19:50 -0400

    [android] updated getFileTypeIcon for search results

--- android/src/com/frostwire/android/gui/adapters/SearchResultListAdapter.java
+++ android/src/com/frostwire/android/gui/adapters/SearchResultListAdapter.java
@@ -236,19 +236,19 @@ public abstract class SearchResultListAdapter extends AbstractListAdapter<Search
     private int getFileTypeIconId() {
         switch (fileType) {
             case Constants.FILE_TYPE_APPLICATIONS:
-                return R.drawable.browse_peer_application_icon_selector_menu;
+                return R.drawable.list_item_application_icon;
             case Constants.FILE_TYPE_AUDIO:
-                return R.drawable.browse_peer_audio_icon_selector_menu;
+                return R.drawable.list_item_audio_icon;
             case Constants.FILE_TYPE_DOCUMENTS:
-                return R.drawable.browse_peer_document_icon_selector_menu;
+                return R.drawable.list_item_document_icon;
             case Constants.FILE_TYPE_PICTURES:
-                return R.drawable.browse_peer_picture_icon_selector_menu;
+                return R.drawable.list_item_picture_icon;
             case Constants.FILE_TYPE_VIDEOS:
-                return R.drawable.browse_peer_video_icon_selector_menu;
+                return R.drawable.list_item_video_icon;
             case Constants.FILE_TYPE_TORRENTS:
-                return R.drawable.browse_peer_torrent_icon_selector_menu;
+                return R.drawable.list_item_torrent_icon;
             default:
-                return R.drawable.question_mark;
+                return R.drawable.list_item_question_mark;
         }
     }
 
