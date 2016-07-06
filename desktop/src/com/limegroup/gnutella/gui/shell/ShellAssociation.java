dd2a386d979e3172dc99d47ff29b02cbccd0a88b
gubatron <gubatron@gmail.com>
2016-07-05 23:52:11 -0600

    [desktop] typo refactor, missing license headers, indentation

--- desktop/src/com/limegroup/gnutella/gui/shell/ShellAssociation.java
+++ desktop/src/com/limegroup/gnutella/gui/shell/ShellAssociation.java
@@ -1,28 +1,41 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.limegroup.gnutella.gui.shell;
 
 /**
  * A registration in the platform shell that sets a program as the default viewer for a protocol link or file type.
  */
-public interface ShellAssociation {
-
+interface ShellAssociation {
 	/**
 	 * @return true if we are currently handling this association
 	 */
-	public boolean isRegistered();
-	
+	boolean isRegistered();
 	/**
 	 * @return true if nobody is handling this association
 	 */
-	public boolean isAvailable();
-
+	boolean isAvailable();
 	/**
 	 * Associates this running program with this protocol or file type in the shell.
 	 */
-	public void register();
-
+	void register();
 	/**
 	 * Clears this shell association, leaving no program registered.
 	 */
-	public void unregister();
-
+	void unregister();
 }
