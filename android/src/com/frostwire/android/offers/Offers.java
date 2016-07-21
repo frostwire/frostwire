6a65191fbfaa7583d41e59cb1ffc2bd3d3f49c5b
Alden Torres <aldenml@gmail.com>
2016-07-21 09:57:31 -0400

    [android] refactored odd parameter debugMode

--- android/src/com/frostwire/android/offers/Offers.java
+++ android/src/com/frostwire/android/offers/Offers.java
@@ -2,18 +2,17 @@
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
  * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.offers;
@@ -37,16 +36,17 @@ import java.util.concurrent.TimeUnit;
 public final class Offers {
 
     private static final Logger LOG = Logger.getLogger(Offers.class);
+    static final boolean DEBUG_MODE = false;
 
     private Offers() {
     }
 
     static final ThreadPool THREAD_POOL = new ThreadPool("SearchManager", 1, 5, 1L, new LinkedBlockingQueue<Runnable>(), true);
     private static long lastInterstitialShownTimestamp = -1;
-    private final static boolean DEBUG_MODE = false;
-    private final static AppLovinAdNetwork APP_LOVIN = new AppLovinAdNetwork(DEBUG_MODE);
-    private final static InMobiAdNetwork IN_MOBI = new InMobiAdNetwork(DEBUG_MODE);
-    private final static RemoveAdsNetwork REMOVE_ADS = new RemoveAdsNetwork(DEBUG_MODE);
+
+    private final static AppLovinAdNetwork APP_LOVIN = new AppLovinAdNetwork();
+    private final static InMobiAdNetwork IN_MOBI = new InMobiAdNetwork();
+    private final static RemoveAdsNetwork REMOVE_ADS = new RemoveAdsNetwork();
     private static List<AdNetwork> AD_NETWORKS;
 
     public static void initAdNetworks(Activity activity) {
