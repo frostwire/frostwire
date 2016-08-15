f508a4255b79c5cd4b68f684fc8452e012f2ef30
gubatron <gubatron@gmail.com>
2016-08-15 17:47:53 -0600

    [desktop] changelog. build 197

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -44,7 +44,7 @@ public final class FrostWireUtils {
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 196;
+    private static final int BUILD_NUMBER = 197;
 
     /**
      * Make sure the constructor can never be called.
