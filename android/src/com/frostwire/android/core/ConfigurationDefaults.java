970ad57ff26519aeb73f502e000f6f2fb1ee81c3
alejandroarturom <alejandroarturom@gmail.com>
2016-09-05 10:47:02 -0300

    [ANDROID] New Search Engine: LimeTorrents.

--- android/src/com/frostwire/android/core/ConfigurationDefaults.java
+++ android/src/com/frostwire/android/core/ConfigurationDefaults.java
@@ -90,6 +90,7 @@ final class ConfigurationDefaults {
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_BITSNOOP, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_TORLOCK, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_TORRENTDOWNLOADS, true);
+        defaultValues.put(Constants.PREF_KEY_SEARCH_USE_LIMETORRENTS, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_EZTV, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_APPIA, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_TPB, true);
