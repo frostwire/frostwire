07d72df2de1ac77b2c854fbb4a9bbf8cb9544999
gubatron <gubatron@gmail.com>
2016-09-08 15:33:47 -0600

    [desktop] fixes #187, playback broken running from source.

--- desktop/src/com/frostwire/gui/player/MediaPlayerWindows.java
+++ desktop/src/com/frostwire/gui/player/MediaPlayerWindows.java
@@ -26,12 +26,9 @@ import com.limegroup.gnutella.util.FrostWireUtils;
 
 public class MediaPlayerWindows extends MediaPlayer {
 
-	protected String getPlayerPath() {
-		String playerPath;
-		
-		boolean isRelease = !FrostWireUtils.getFrostWireJarPath().contains("frostwire-desktop");
-
-		playerPath = (isRelease) ? FrostWireUtils.getFrostWireJarPath() + File.separator + "fwplayer.exe" : "lib/native/fwplayer.exe";
+    protected String getPlayerPath() {
+        boolean isRelease = !FrostWireUtils.getFrostWireJarPath().contains("desktop\\build\\libs");
+	String playerPath = (isRelease) ? FrostWireUtils.getFrostWireJarPath() + File.separator + "fwplayer.exe" : "lib/native/fwplayer.exe";
         playerPath = decode(playerPath);
 
         if (!new File(playerPath).exists()) {
