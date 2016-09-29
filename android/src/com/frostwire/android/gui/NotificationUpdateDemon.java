dd5fb09a0c51442b4c24cbb59f6a65d54e06475b
gubatron <gubatron@gmail.com>
2016-09-29 00:33:26 -0600

    [android] player notification in white background

--- android/src/com/frostwire/android/gui/NotificationUpdateDemon.java
+++ android/src/com/frostwire/android/gui/NotificationUpdateDemon.java
@@ -1,3 +1,21 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.frostwire.android.gui;
 
 import android.app.Notification;
@@ -21,6 +39,10 @@ import com.frostwire.android.gui.views.TimerService;
 import com.frostwire.android.gui.views.TimerSubscription;
 import com.frostwire.util.Logger;
 
+/**
+ * @author gubatron
+ * @author aldenml
+ */
 public class NotificationUpdateDemon implements TimerObserver {
 
     private static final Logger LOG = Logger.getLogger(NotificationUpdateDemon.class);
