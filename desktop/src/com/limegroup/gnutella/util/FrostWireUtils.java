ec19df8efb8f3f0e116da872129b86a64d279b22
gubatron <gubatron@gmail.com>
2016-10-17 12:27:25 -0600

    [desktop] 6.3.7 becomes 6.4.0, build 207

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,12 +38,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.3.7";
+    private static final String FROSTWIRE_VERSION = "6.4.0";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 206;
+    private static final int BUILD_NUMBER = 207;
 
     /**
      * Make sure the constructor can never be called.
