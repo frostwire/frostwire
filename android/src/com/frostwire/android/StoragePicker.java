2c091d713579db33de3e75f42556b08169e5c63e
Grzesiek Rząca <grzesiek.rzaca@gmail.com>
2016-12-16 14:53:16 +0100

    [android] make activity starting request code smaller than xFFFF for compatibility

--- android/src/com/frostwire/android/StoragePicker.java
+++ android/src/com/frostwire/android/StoragePicker.java
@@ -40,7 +40,7 @@ public final class StoragePicker {
 
     public static final String ACTION_OPEN_DOCUMENT_TREE = "android.intent.action.OPEN_DOCUMENT_TREE";
 
-    public static final int SELECT_FOLDER_REQUEST_CODE = 1267123;
+    public static final int SELECT_FOLDER_REQUEST_CODE = 126;
 
     private static final String EXTRA_SHOW_ADVANCED = "android.content.extra.SHOW_ADVANCED";
 
