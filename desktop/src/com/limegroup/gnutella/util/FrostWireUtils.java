4e5357b5e5c5a5854731ffbd71f5d6142762dba0
gubatron <gubatron@gmail.com>
2016-12-16 00:04:42 -0700

    [desktop] 6.4.3 build 213

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,12 +38,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.4.2";
+    private static final String FROSTWIRE_VERSION = "6.4.3";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 212;
+    private static final int BUILD_NUMBER = 213;
 
     /**
      * Make sure the constructor can never be called.
