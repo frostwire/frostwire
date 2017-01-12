4a536bcbfbc93c24913d88a9689512882c3ed835
Grzesiek Rząca <grzesiek.rzaca@gmail.com>
2017-01-12 15:10:33 +0100

    [android] remove default style constructor

--- android/src/com/frostwire/android/gui/views/preference/NumberPickerPreference2.java
+++ android/src/com/frostwire/android/gui/views/preference/NumberPickerPreference2.java
@@ -43,8 +43,8 @@ public class NumberPickerPreference2 extends DialogPreference {
     private int endRange;
     private int defaultValue;
 
-    public NumberPickerPreference2(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
+    public NumberPickerPreference2(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
         if (attrs == null) {
             return;
@@ -56,28 +56,6 @@ public class NumberPickerPreference2 extends DialogPreference {
         endRange = arr.getInteger(R.styleable.numberpicker_picker_endRange, 200);
         defaultValue = arr.getInteger(R.styleable.numberpicker_picker_defaultValue, 0);
         arr.recycle();
-    }
-
-    public NumberPickerPreference2(Context context, AttributeSet attrs) {
-        this(context, attrs, android.R.attr.dialogPreferenceStyle);
-    }
-
-    public NumberPickerPreference2(Context context) {
-        this(context, null);
-    }
-
-    @Override
-    public void onBindViewHolder(PreferenceViewHolder holder) {
-        super.onBindViewHolder(holder);
-        //fixing code error that doesn't hide properly the layout that holds the icon
-        //can't find parent of icon directly for some reason so we find the icon and set it's parent visibility
-        final ImageView imageView = (ImageView) holder.findViewById(android.R.id.icon);
-        if (imageView != null) {
-            ViewGroup parent = (LinearLayout) imageView.getParent();
-            if (parent != null) {
-                parent.setVisibility(getIcon() != null ? View.VISIBLE : View.GONE);
-            }
-        }
     }
 
     protected void saveValue(long val) {
