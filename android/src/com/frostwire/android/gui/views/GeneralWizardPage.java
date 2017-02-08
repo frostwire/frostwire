f806561feb863399e4dcfa869294d4caf82fc486
Alden Torres <aldenml@gmail.com>
2017-02-08 15:23:49 -0500

    [android] refactor in GeneralWizardPage and minor string translation fix

--- android/src/com/frostwire/android/gui/views/GeneralWizardPage.java
+++ android/src/com/frostwire/android/gui/views/GeneralWizardPage.java
@@ -1,19 +1,18 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.gui.views;
@@ -23,14 +22,17 @@ import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.*;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.frostwire.android.AndroidPlatform;
 import com.frostwire.android.R;
+import com.frostwire.android.StoragePicker;
 import com.frostwire.android.core.ConfigurationManager;
 import com.frostwire.android.core.Constants;
-import com.frostwire.android.gui.views.preference.StoragePreference;
-import com.frostwire.platform.Platforms;
 
 /**
  * @author gubatron
@@ -97,7 +99,12 @@ public class GeneralWizardPage extends RelativeLayout implements WizardPageView 
         TextView titleHorizontalBreak = (TextView) findViewById(R.id.view_general_wizard_page_title_horizontal_break);
 
         if (AndroidPlatform.saf()) {
-            textStoragePath.setOnClickListener(new StoragePathTextViewAdapter((Activity) getContext()));
+            textStoragePath.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    StoragePicker.show((Activity) getContext());
+                }
+            });
         } else {
             titleHorizontalBreak.setVisibility(View.GONE);
             textStoragePathTitle.setVisibility(View.GONE);
@@ -145,16 +152,5 @@ public class GeneralWizardPage extends RelativeLayout implements WizardPageView 
      */
     private void validate() {
         onComplete(true);
-    }
-
-    private static class StoragePathTextViewAdapter extends ClickAdapter<Activity> {
-        public StoragePathTextViewAdapter(Activity owner) {
-            super(owner);
-        }
-
-        @Override
-        public void onClick(Activity owner, View v) {
-            StoragePreference.invokeStoragePreference(owner);
-        }
     }
 }
