9ba8871a170f922bcb9a746ed294cb1900254be4
Alden Torres <aldenml@gmail.com>
2017-02-09 15:33:53 -0500

    [android] cleanup refactor in RepeatButton

--- android/apollo/src/com/andrew/apollo/widgets/RepeatButton.java
+++ android/apollo/src/com/andrew/apollo/widgets/RepeatButton.java
@@ -1,12 +1,19 @@
 /*
- * Copyright (C) 2012 Andrew Neal Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2012 Andrew Neal
+ * Modified by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2013-2017, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.andrew.apollo.widgets;
@@ -20,46 +27,24 @@ import android.view.View.OnLongClickListener;
 import android.widget.ImageButton;
 
 import com.andrew.apollo.MusicPlaybackService;
-import com.frostwire.android.R;
 import com.andrew.apollo.utils.ApolloUtils;
 import com.andrew.apollo.utils.MusicUtils;
-import com.andrew.apollo.utils.ThemeUtils;
+import com.frostwire.android.R;
 
 /**
  * A custom {@link ImageButton} that represents the "repeat" button.
- * 
+ *
  * @author Andrew Neal (andrewdneal@gmail.com)
  */
-public class RepeatButton extends ImageButton implements OnClickListener, OnLongClickListener {
-
-    /**
-     * Repeat one theme resource
-     */
-    private static final String REPEAT_ALL = "btn_playback_repeat_all";
-
-    /**
-     * Repeat one theme resource
-     */
-    private static final String REPEAT_CURRENT = "btn_playback_repeat_one";
-
-    /**
-     * Repeat one theme resource
-     */
-    private static final String REPEAT_NONE = "btn_playback_repeat";
-
-    /**
-     * The resources to use.
-     */
-    private final ThemeUtils mResources;
+public final class RepeatButton extends ImageButton
+        implements OnClickListener, OnLongClickListener {
 
     /**
      * @param context The {@link Context} to use
-     * @param attrs The attributes of the XML tag that is inflating the view.
+     * @param attrs   The attributes of the XML tag that is inflating the view.
      */
     public RepeatButton(final Context context, final AttributeSet attrs) {
         super(context, attrs);
-        // Initialze the theme resources
-        mResources = new ThemeUtils(context);
         setBackgroundResource(R.drawable.holo_background_selector);
         // Control playback (cycle repeat modes)
         setOnClickListener(this);
@@ -67,18 +52,12 @@ public class RepeatButton extends ImageButton implements OnClickListener, OnLong
         setOnLongClickListener(this);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void onClick(final View v) {
         MusicUtils.cycleRepeat();
         updateRepeatState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public boolean onLongClick(final View view) {
         if (TextUtils.isEmpty(view.getContentDescription())) {
@@ -96,15 +75,15 @@ public class RepeatButton extends ImageButton implements OnClickListener, OnLong
         switch (MusicUtils.getRepeatMode()) {
             case MusicPlaybackService.REPEAT_ALL:
                 setContentDescription(getResources().getString(R.string.accessibility_repeat_all));
-                setImageDrawable(mResources.getDrawable(REPEAT_ALL));
+                setImageResource(R.drawable.btn_playback_repeat_all);
                 break;
             case MusicPlaybackService.REPEAT_CURRENT:
                 setContentDescription(getResources().getString(R.string.accessibility_repeat_one));
-                setImageDrawable(mResources.getDrawable(REPEAT_CURRENT));
+                setImageResource(R.drawable.btn_playback_repeat_one);
                 break;
             case MusicPlaybackService.REPEAT_NONE:
                 setContentDescription(getResources().getString(R.string.accessibility_repeat));
-                setImageDrawable(mResources.getDrawable(REPEAT_NONE));
+                setImageResource(R.drawable.btn_playback_repeat);
                 break;
             default:
                 break;
