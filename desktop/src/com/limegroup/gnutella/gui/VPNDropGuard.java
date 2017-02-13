a9bafd3ef4cf1b898bec4c043e415788d904f51d
gubatron <gubatron@gmail.com>
2017-02-13 12:17:23 -0700

    [desktop] make sure explanation dialog is created in dispatch thread

--- desktop/src/com/limegroup/gnutella/gui/VPNDropGuard.java
+++ desktop/src/com/limegroup/gnutella/gui/VPNDropGuard.java
@@ -43,55 +43,61 @@ public class VPNDropGuard {
     }
 
     private static void showExplanationDialog() {
-        final JDialog dialog = new JDialog();
-        dialog.setTitle(tr("VPN-Drop Protection Active"));
-        dialog.setLayout(new FlowLayout()); // gets rid of that default border layout
-        dialog.setModal(true);
-        dialog.setResizable(false);
-        JPanel panel = new JPanel(new MigLayout());
-        panel.add(new JLabel("<html><h1>" + tr("BitTorrent is off because your VPN is disconnected") + "</h1></html>"), "wrap");
-        panel.add(new JLabel("<html><h2>" + tr("Check the status of your VPN connection or disable the VPN-Drop Protection") + ".</h2></html>"), "wrap");
-        JButton whatIsAVPN = new JButton("<html><h3>" + tr("What is a VPN?"));
-        whatIsAVPN.addActionListener(new ActionListener() {
+        GUIMediator.safeInvokeLater(new Runnable() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();
-                GUIMediator.openURL(VPNStatusButton.VPN_URL);
-            }
-        });
-        JButton disableVPNDrop = new JButton("<html><h3>" + tr("Disable VPN-Drop protection"));
-        disableVPNDrop.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();
-                ConnectionSettings.VPN_DROP_PROTECTION.setValue(false);
-                MessageService.instance().showMessage(tr("VPN-Drop protection disabled. Restarting BitTorrent engine."));
-                BackgroundExecutorService.schedule(new Runnable() {
+            public void run() {
+                final JDialog dialog = new JDialog();
+                dialog.setTitle(tr("VPN-Drop Protection Active"));
+                dialog.setLayout(new FlowLayout()); // gets rid of that default border layout
+                dialog.setModal(true);
+                dialog.setResizable(false);
+                JPanel panel = new JPanel(new MigLayout());
+                panel.add(new JLabel("<html><h1>" + tr("BitTorrent is off because your VPN is disconnected") + "</h1></html>"), "wrap");
+                panel.add(new JLabel("<html><h2>" + tr("Check the status of your VPN connection or disable the VPN-Drop Protection") + ".</h2></html>"), "wrap");
+                JButton whatIsAVPN = new JButton("<html><h3>" + tr("What is a VPN?"));
+                whatIsAVPN.addActionListener(new ActionListener() {
                     @Override
-                    public void run() {
-                        BTEngine.getInstance().restart(); // has a sleep call, don't do this on UI thread.
+                    public void actionPerformed(ActionEvent e) {
+                        dialog.dispose();
+                        GUIMediator.openURL(VPNStatusButton.VPN_URL);
                     }
                 });
-            }
-        });
-        JButton ok = new JButton("<html><h3>" + tr("Ok"));
-        ok.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();
-            }
-        });
-        JPanel panelButtons = new JPanel(new MigLayout("","[][][grow]"));
-        panelButtons.add(whatIsAVPN);
-        panelButtons.add(disableVPNDrop);
-        panelButtons.add(ok,"growx, shrink 0");
-        panel.add(panelButtons, "growx");
-        dialog.add(panel);
+                JButton disableVPNDrop = new JButton("<html><h3>" + tr("Disable VPN-Drop protection"));
+                disableVPNDrop.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        dialog.dispose();
+                        ConnectionSettings.VPN_DROP_PROTECTION.setValue(false);
+                        MessageService.instance().showMessage(tr("VPN-Drop protection disabled. Restarting BitTorrent engine."));
+                        BackgroundExecutorService.schedule(new Runnable() {
+                            @Override
+                            public void run() {
+                                BTEngine.getInstance().restart(); // has a sleep call, don't do this on UI thread.
+                            }
+                        });
+                    }
+                });
+                JButton ok = new JButton("<html><h3>" + tr("Ok"));
+                ok.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        dialog.dispose();
+                    }
+                });
+                JPanel panelButtons = new JPanel(new MigLayout("","[][][grow]"));
+                panelButtons.add(whatIsAVPN);
+                panelButtons.add(disableVPNDrop);
+                panelButtons.add(ok,"growx, shrink 0");
+                panel.add(panelButtons, "growx");
+                dialog.add(panel);
 
-        dialog.setPreferredSize(new Dimension(752, 214));
-        dialog.pack();
-        dialog.setLocationRelativeTo(GUIMediator.getAppFrame());
-        dialog.setVisible(true);
+                dialog.setPreferredSize(new Dimension(752, 214));
+                dialog.pack();
+                dialog.setLocationRelativeTo(GUIMediator.getAppFrame());
+                dialog.setVisible(true);
+            }
+        });
+
     }
 
     public static boolean canUseBitTorrent() {
--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -43,7 +43,7 @@ public final class FrostWireUtils {
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 216;
+    private static final int BUILD_NUMBER = 217;
 
     /**
      * Make sure the constructor can never be called.
