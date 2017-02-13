f36381eac8ffabd47ffedab7e26125504b0206bb
Alden Torres <aldenml@gmail.com>
2017-02-13 13:33:11 -0500

    [android] cleanup and formatting

--- android/apollo/src/com/andrew/apollo/ui/fragments/ArtistFragment.java
+++ android/apollo/src/com/andrew/apollo/ui/fragments/ArtistFragment.java
@@ -1,26 +1,32 @@
 /*
- * Copyright (C) 2012 Andrew Neal Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2012 Andrew Neal
+ * Modified by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2013-2017, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.andrew.apollo.ui.fragments;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.app.Fragment;
 import android.content.Loader;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+
 import com.andrew.apollo.adapters.ArtistAdapter;
 import com.andrew.apollo.loaders.ArtistLoader;
 import com.andrew.apollo.model.Artist;
-import com.andrew.apollo.ui.activities.BaseActivity;
 import com.andrew.apollo.ui.fragments.profile.ApolloFragment;
 import com.andrew.apollo.utils.MusicUtils;
 import com.andrew.apollo.utils.NavUtils;
@@ -34,27 +40,18 @@ import static com.andrew.apollo.utils.PreferenceUtils.ARTIST_LAYOUT;
 
 /**
  * This class is used to display all of the artists on a user's device.
- * 
+ *
  * @author Andrew Neal (andrewdneal@gmail.com)
  * @author Angel Leon (@gubatron)
  * @author Alden Torres (@aldenml)
  */
-public class ArtistFragment extends ApolloFragment<ArtistAdapter, Artist> {
-
-    //private static Logger LOGGER = Logger.getLogger(ArtistFragment.class);
+public final class ArtistFragment extends ApolloFragment<ArtistAdapter, Artist> {
 
     /**
      * Empty constructor as per the {@link Fragment} documentation
      */
     public ArtistFragment() {
         super(Fragments.ARTIST_FRAGMENT_GROUP_ID, Fragments.ARTIST_FRAGMENT_LOADER_ID);
-    }
-
-    @Override
-    public void onAttach(final Activity activity) {
-        super.onAttach(activity);
-        // Register the music status listener
-        ((BaseActivity)activity).setMusicStateListenerListener(this);
     }
 
     @Override
@@ -86,7 +83,7 @@ public class ArtistFragment extends ApolloFragment<ArtistAdapter, Artist> {
 
     @Override
     public void onItemClick(final AdapterView<?> parent, final View view, final int position,
-            final long id) {
+                            final long id) {
         mItem = mAdapter.getItem(position);
         long[] tracks = MusicUtils.getSongListForArtist(getActivity(), mItem.mArtistId);
         NavUtils.openArtistProfile(getActivity(), mItem.mArtistName, tracks);
@@ -115,7 +112,7 @@ public class ArtistFragment extends ApolloFragment<ArtistAdapter, Artist> {
 
     /**
      * @return The position of an item in the list or grid based on the name of
-     *         the currently playing artist.
+     * the currently playing artist.
      */
     private int getItemPositionByArtist() {
         final long artistId = MusicUtils.getCurrentArtistId();
--- android/apollo/src/com/andrew/apollo/ui/fragments/LastAddedFragment.java
+++ android/apollo/src/com/andrew/apollo/ui/fragments/LastAddedFragment.java
@@ -1,21 +1,29 @@
 /*
- * Copyright (C) 2015 Angel Leon, Alden Torres, Andrew Neal Licensed under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Copyright (C) 2012 Andrew Neal
+ * Modified by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2013-2017, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.andrew.apollo.ui.fragments;
 
-import android.os.Bundle;
 import android.app.Fragment;
 import android.content.Loader;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+
 import com.andrew.apollo.adapters.SongAdapter;
 import com.andrew.apollo.loaders.LastAddedLoader;
 import com.andrew.apollo.model.Song;
@@ -33,7 +41,8 @@ import java.util.List;
  * @author Angel Leon (@gubatron)
  * @author Alden Torres (@aldenml)
  */
-public class LastAddedFragment extends ApolloFragment<SongAdapter, Song> {
+public final class LastAddedFragment extends ApolloFragment<SongAdapter, Song> {
+
     /**
      * Empty constructor as per the {@link Fragment} documentation
      */
