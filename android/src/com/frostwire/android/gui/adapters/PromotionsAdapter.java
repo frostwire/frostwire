da274154c55bb7d2dc675d16192f3131c167e7df
Alden Torres <aldenml@gmail.com>
2017-04-27 14:53:33 -0400

    [android] formatting and header in PromotionsAdapter

--- android/src/com/frostwire/android/gui/adapters/PromotionsAdapter.java
+++ android/src/com/frostwire/android/gui/adapters/PromotionsAdapter.java
@@ -1,19 +1,19 @@
 /*
- * Created by Angel Leon (@gubatron), Alden Torres (aldenml), Marcelina Knitter (@marcelinkaaa)
- * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ *            Marcelina Knitter (@marcelinkaaa)
+ * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.gui.adapters;
@@ -75,7 +75,7 @@ public class PromotionsAdapter extends AbstractAdapter<Slide> {
         ImageView imageView = findView(convertView, R.id.view_promotions_item_image);
         TextView downloadTextView = findView(convertView, R.id.view_promotions_item_download_textview);
         ImageView previewImageView = findView(convertView, R.id.view_promotions_item_preview_imageview);
-        ImageView readmoreImageView =  findView(convertView, R.id.view_promotions_item_readmore_imageview);
+        ImageView readmoreImageView = findView(convertView, R.id.view_promotions_item_readmore_imageview);
 
         GridView gridView = (GridView) parent;
         int promoWidth = gridView.getColumnWidth();
@@ -136,7 +136,7 @@ public class PromotionsAdapter extends AbstractAdapter<Slide> {
     private void startVideoPreview(String videoURL) {
         // frostwire-preview is not a mobile friendly experience, let's take them straight to youtube
         if (videoURL.startsWith(Constants.FROSTWIRE_PREVIEW_DOT_COM_URL)) {
-            videoURL = videoURL.substring(videoURL.indexOf("detailsUrl=")+"detailsUrl=".length());
+            videoURL = videoURL.substring(videoURL.indexOf("detailsUrl=") + "detailsUrl=".length());
         }
 
         Intent i = new Intent(Intent.ACTION_VIEW);
