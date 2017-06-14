3597d39ddb386ae94b360699a0bcf33c9a0b643c
Alden Torres <aldenml@gmail.com>
2017-06-14 19:46:51 -0400

    [common] avoid re-download of torrent is already complete

--- common/src/main/java/com/frostwire/bittorrent/BTEngine.java
+++ common/src/main/java/com/frostwire/bittorrent/BTEngine.java
@@ -2,18 +2,17 @@
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
  * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.bittorrent;
@@ -52,6 +51,7 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -259,6 +259,11 @@ public final class BTEngine extends SessionManager {
 
         TorrentInfo ti = new TorrentInfo(torrent);
 
+        if (selection == null) {
+            selection = new boolean[ti.numFiles()];
+            Arrays.fill(selection, true);
+        }
+
         Priority[] priorities = null;
 
         TorrentHandle th = find(ti.infoHash());
@@ -271,10 +276,18 @@ public final class BTEngine extends SessionManager {
                 priorities = Priority.array(Priority.IGNORE, ti.numFiles());
             }
 
+            boolean changed = false;
             for (int i = 0; i < selection.length; i++) {
                 if (selection[i]) {
-                    priorities[i] = Priority.NORMAL;
+                    if (priorities[i] == Priority.IGNORE) {
+                        priorities[i] = Priority.NORMAL;
+                        changed = true;
+                    }
                 }
+            }
+
+            if (!changed) { // nothing to do
+                return;
             }
         }
 
@@ -299,6 +312,11 @@ public final class BTEngine extends SessionManager {
             return;
         }
 
+        if (selection == null) {
+            selection = new boolean[ti.numFiles()];
+            Arrays.fill(selection, true);
+        }
+
         Priority[] priorities = null;
 
         TorrentHandle th = find(ti.infoHash());
@@ -312,10 +330,18 @@ public final class BTEngine extends SessionManager {
             }
 
             if (priorities != null) {
+                boolean changed = false;
                 for (int i = 0; i < selection.length; i++) {
                     if (selection[i] && i < priorities.length) {
-                        priorities[i] = Priority.NORMAL;
+                        if (priorities[i] == Priority.IGNORE) {
+                            priorities[i] = Priority.NORMAL;
+                            changed = true;
+                        }
                     }
+                }
+
+                if (!changed) { // nothing to do
+                    return;
                 }
             }
         }
