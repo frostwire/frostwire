4d9a328234766bc815c36b959a4088bdcf32df85
Alden Torres <aldenml@gmail.com>
2017-06-14 13:27:54 -0400

    [android] headers and formatting

--- android/src/com/frostwire/android/gui/views/AbstractDialog.java
+++ android/src/com/frostwire/android/gui/views/AbstractDialog.java
@@ -1,20 +1,19 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
- * Marcelina Knitter (@marcelinkaaa)
+ *            Marcelina Knitter (@marcelinkaaa)
  * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.gui.views;
@@ -40,10 +39,11 @@ import java.util.List;
 
 /**
  * IMPORTANT:
- *  - All subclasses must be public, otherwise the dialogs can't be instantiated by android on rotation
- *  - All subclasses must only have an empty constructor. If you feel the need to use a custom constructor
- *    implement a "newInstance(...)" method that uses the default empty constructor and then set your
- *    attributes on the object to return.
+ * - All subclasses must be public, otherwise the dialogs can't be instantiated by android on rotation
+ * - All subclasses must only have an empty constructor. If you feel the need to use a custom constructor
+ * implement a "newInstance(...)" method that uses the default empty constructor and then set your
+ * attributes on the object to return.
+ *
  * @author gubatron
  * @author aldenml
  * @author marcelinkaaa
