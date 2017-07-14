1d231dda574bb7b928c150c8cc20baf4c7098e17
Alden Torres <aldenml@gmail.com>
2017-07-14 10:19:56 -0400

    [android] code cleanup and header in HandpickedTorrentDownloadDialog

--- android/src/com/frostwire/android/gui/dialogs/HandpickedTorrentDownloadDialog.java
+++ android/src/com/frostwire/android/gui/dialogs/HandpickedTorrentDownloadDialog.java
@@ -1,19 +1,18 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.gui.dialogs;
@@ -37,7 +36,6 @@ import com.frostwire.jlibtorrent.TcpEndpoint;
 import com.frostwire.jlibtorrent.TorrentInfo;
 import com.frostwire.jlibtorrent.swig.add_torrent_params;
 import com.frostwire.jlibtorrent.swig.error_code;
-import com.frostwire.jlibtorrent.swig.libtorrent;
 import com.frostwire.jlibtorrent.swig.tcp_endpoint_vector;
 import com.frostwire.util.JsonUtils;
 import com.frostwire.util.Logger;
@@ -53,15 +51,13 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * Created on 4/19/16.
- *
  * @author gubatron
  * @author aldenml
  */
-@SuppressWarnings("WeakerAccess")
-// We need the class to be public so that the dialog can be rotated (via Reflection)
-public class HandpickedTorrentDownloadDialog extends AbstractConfirmListDialog<HandpickedTorrentDownloadDialog.TorrentFileEntry> {
-    private static Logger LOG = Logger.getLogger(HandpickedTorrentDownloadDialog.class);
+public final class HandpickedTorrentDownloadDialog extends AbstractConfirmListDialog<HandpickedTorrentDownloadDialog.TorrentFileEntry> {
+
+    private static final Logger LOG = Logger.getLogger(HandpickedTorrentDownloadDialog.class);
+
     private TorrentInfo torrentInfo;
     private String magnetUri;
     private static final String BUNDLE_KEY_TORRENT_INFO_DATA = "torrentInfoData";
