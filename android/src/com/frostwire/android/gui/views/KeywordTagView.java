70329ca9218fb9b3247868930e3e371d2e537464
Alden Torres <aldenml@gmail.com>
2017-07-17 11:10:11 -0400

    [android] using spannable string to multi format string in KeywordTagView
    this avoids the need of two TextView, render cost is about the same, but measure of components
    and performance is improved

--- android/src/com/frostwire/android/gui/views/KeywordTagView.java
+++ android/src/com/frostwire/android/gui/views/KeywordTagView.java
@@ -24,6 +24,9 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.support.v4.content.ContextCompat;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.TextAppearanceSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageButton;
@@ -48,6 +51,8 @@ public class KeywordTagView extends LinearLayout {
     private KeywordFilter keywordFilter;
     private int count;
     private KeywordTagViewListener listener;
+    private TextAppearanceSpan keywordSpan;
+    private TextAppearanceSpan countSpan;
 
     public interface KeywordTagViewListener {
         void onKeywordTagViewDismissed(KeywordTagView view);
@@ -75,6 +80,10 @@ public class KeywordTagView extends LinearLayout {
         setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         setVisibility(View.VISIBLE);
         inflate(context, R.layout.view_keyword_tag, this);
+
+        // TODO: refactor and move logic to onLayoutInflate
+        keywordSpan = new TextAppearanceSpan(getContext(), R.style.keywordTagText);
+        countSpan = new TextAppearanceSpan(getContext(), R.style.keywordTagCount);
     }
 
     public KeywordTagView(Context context, AttributeSet attrs) {
@@ -92,15 +101,16 @@ public class KeywordTagView extends LinearLayout {
     private void updateComponents() {
         ImageButton inclusiveIndicatorImageView = (ImageButton) findViewById(R.id.view_keyword_tag_inclusive_indicator);
         TextView keywordTextView = (TextView) findViewById(R.id.view_keyword_tag_keyword);
-        TextView countTextView = (TextView) findViewById(R.id.view_keyword_tag_count);
         LinearLayout tagContainer = (LinearLayout) findViewById(R.id.view_keyword_tag_container);
-        if (count == -1) {
-            countTextView.setVisibility(View.GONE);
-        }
         ImageView dismissTextView = (ImageView) findViewById(R.id.view_keyword_tag_dismiss);
         inclusiveIndicatorImageView.setImageResource(keywordFilter.isInclusive() ? R.drawable.filter_add : R.drawable.filter_minus);
-        keywordTextView.setText(keywordFilter.getKeyword());
-        countTextView.setText("(" + String.valueOf(count) + ")");
+
+        SpannableStringBuilder sb = new SpannableStringBuilder();
+        sb = append(sb, keywordFilter.getKeyword(), keywordSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        if (count != -1) {
+            sb = append(sb, "  (" + String.valueOf(count) + ")", countSpan, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        keywordTextView.setText(sb, TextView.BufferType.SPANNABLE);
 
         if (isInEditMode()) {
             return;
@@ -134,7 +144,6 @@ public class KeywordTagView extends LinearLayout {
         };
         inclusiveIndicatorImageView.setOnClickListener(onClickListener);
         keywordTextView.setOnClickListener(onClickListener);
-        countTextView.setOnClickListener(onClickListener);
     }
 
     private void onKeywordTagViewTouched() {
@@ -174,5 +183,13 @@ public class KeywordTagView extends LinearLayout {
                 LOG.error(t.getMessage(), t);
             }
         }
+    }
+
+    // for API 16 compatibility
+    private static SpannableStringBuilder append(SpannableStringBuilder sb, CharSequence text, Object what, int flags) {
+        int start = sb.length();
+        sb.append(text);
+        sb.setSpan(what, start, sb.length(), flags);
+        return sb;
     }
 }
