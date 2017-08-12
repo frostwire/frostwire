3a26afb27cb128a9ccfb0dc044b84978b52356f9
Alden Torres <aldenml@gmail.com>
2017-08-12 14:17:29 -0400

    [android] refactor in CopyToClipboardMenuAction to be able to reuse it

--- android/src/com/frostwire/android/gui/adapters/menu/CopyToClipboardMenuAction.java
+++ android/src/com/frostwire/android/gui/adapters/menu/CopyToClipboardMenuAction.java
@@ -1,19 +1,19 @@
 /*
- * Created by Angel Leon (@gubatron), Alden Torres (aldenml), Jose Molina (@votaguz)
- * Copyright (c) 2011-2015, FrostWire(R). All rights reserved.
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
+ *            Jose Molina (@votaguz)
+ * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.gui.adapters.menu;
@@ -30,7 +30,7 @@ import com.frostwire.android.gui.views.MenuAction;
  * @author aldenml
  * @author votaguz
  */
-public final class CopyToClipboardMenuAction extends MenuAction {
+public class CopyToClipboardMenuAction extends MenuAction {
 
     private final int messageId;
     private final Object data;
@@ -46,11 +46,16 @@ public final class CopyToClipboardMenuAction extends MenuAction {
     protected void onClick(Context context) {
         ClipboardManager clipboard = (ClipboardManager)
                 context.getSystemService(Context.CLIPBOARD_SERVICE);
+        Object data = getData();
 
         if (clipboard != null && data != null) {
-            ClipData clip = ClipData.newPlainText("data", this.data.toString());
+            ClipData clip = ClipData.newPlainText("data", data.toString());
             clipboard.setPrimaryClip(clip);
-            UIUtils.showLongMessage(context, this.messageId);
+            UIUtils.showLongMessage(context, messageId);
         }
+    }
+
+    protected Object getData() {
+        return data;
     }
 }
