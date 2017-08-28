5abe69ae1d4f168c7b71b776971be1e05ee28f21
gubatron <gubatron@gmail.com>
2017-08-28 14:10:43 -0600

    [android] Fixes on StopSeedAction

--- android/src/com/frostwire/android/gui/adapters/menu/StopSeedingAction.java
+++ android/src/com/frostwire/android/gui/adapters/menu/StopSeedingAction.java
@@ -1,5 +1,6 @@
 /*
- * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
+ * Marcelina Knitter (@marcelinkaaa)
  * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,8 +21,6 @@ package com.frostwire.android.gui.adapters.menu;
 import android.content.Context;
 
 import com.frostwire.android.R;
-import com.frostwire.android.core.ConfigurationManager;
-import com.frostwire.android.core.FileDescriptor;
 import com.frostwire.android.gui.transfers.TransferManager;
 import com.frostwire.android.gui.util.UIUtils;
 import com.frostwire.android.gui.views.AbstractDialog;
@@ -37,53 +36,42 @@ import com.frostwire.util.Logger;
  * @author aldenml
  */
 public class StopSeedingAction extends MenuAction implements AbstractDialog.OnDialogClickListener {
-
     private static final Logger LOG = Logger.getLogger(StopSeedingAction.class);
-    private final FileDescriptor fd;
     private final BittorrentDownload btDownload;
     private final Transfer transferToClear;
 
-    // TODO: Receive extra metadata that could be put/used in the torrent for
-    // enriched announcement.
-
     private StopSeedingAction(Context context,
-                              FileDescriptor fd,
                               BittorrentDownload existingBittorrentDownload,
                               Transfer transferToClear) {
         super(context, R.drawable.contextmenu_icon_seed, R.string.seed_stop);
-        this.fd = fd;
         this.btDownload = existingBittorrentDownload;
         this.transferToClear = transferToClear;
     }
 
     @Override
     protected void onClick(Context context) {
-        if (ConfigurationManager.instance().isSeedFinishedTorrents()) {
-            stopSeedingEm();
-            UIUtils.showTransfersOnDownloadStart(getContext());
-        }
+        stopSeedingEm();
+        UIUtils.showTransfersOnDownloadStart(getContext());
     }
 
     public StopSeedingAction(Context context, BittorrentDownload download) {
-        this(context, null, download, null);
+        this(context, download, null);
     }
 
     private void stopSeedingEm() {
-        if (!TransferManager.instance().isMobileAndDataSavingsOn()) {
-            if (btDownload != null) {
-                stopSeedingBTDownload();
-            }
-
-            if (transferToClear != null) {
-                TransferManager.instance().remove(transferToClear);
-            }
+        if (btDownload != null) {
+            stopSeedingBTDownload();
+        }
+        if (transferToClear != null) {
+            TransferManager.instance().remove(transferToClear);
         }
     }
 
     private void stopSeedingBTDownload() {
-        btDownload.pause();
         final Object torrentHandle = BTEngine.getInstance().find(new Sha1Hash(btDownload.getInfoHash()));
-        if (torrentHandle == null) {
+        if (torrentHandle != null) {
+            btDownload.pause();
+        } else {
             LOG.warn("stopSeedingBTDownload() could not find torrentHandle for existing torrent.");
         }
     }
