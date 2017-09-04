109dac8bef1371b18bd096edfede136fa108dee9
gubatron <gubatron@gmail.com>
2017-09-03 22:11:18 -0600

    [android] cleanup

--- android/src/com/frostwire/android/gui/views/TransfersNoSeedsView.java
+++ android/src/com/frostwire/android/gui/views/TransfersNoSeedsView.java
@@ -83,13 +83,11 @@ public class TransfersNoSeedsView extends LinearLayout {
                 topTextView.setText(R.string.seeding_is_currently_disabled);
                 midBoldTextView.setText(R.string.seeding_keeps_the_network_healthy);
                 button.setText(R.string.enable_seeding);
-                setVisibility(VISIBLE);
                 break;
             case SEED_ALL_FINISHED:
                 topTextView.setText(R.string.nothing_is_currently_seeding);
                 midBoldTextView.setText(R.string.would_you_like_to_seed_your_finished_torrent_transfers);
                 button.setText(R.string.seed_all_finished);
-                setVisibility(VISIBLE);
                 break;
         }
     }
