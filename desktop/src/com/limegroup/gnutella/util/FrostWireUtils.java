c9f700b95052b9fa9bda94a97b5f3a8e3997ef3e
gubatron <gubatron@gmail.com>
2017-09-22 05:59:16 -0600

    [desktop] 6.5.7 build 244

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,12 +38,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.5.6";
+    private static final String FROSTWIRE_VERSION = "6.5.7";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 243;
+    private static final int BUILD_NUMBER = 244;
 
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
