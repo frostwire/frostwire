0c9ec2b5d35ea35e360fbf284d98303c65a538fc
Marcelina Knitter <knitter.marcelina@gmail.com>
2017-09-22 13:26:31 -0600

    [android] added toolbar menu to TransferDetails

---
+++ android/src/com/frostwire/android/gui/fragments/TransferDetailFragment.java
@@ -0,0 +1,47 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
+ *            Marcelina Knitter (@marcelinkaaa), Jose Molina (@votaguz)
+ * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.frostwire.android.gui.fragments;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import com.frostwire.android.R;
+import com.frostwire.android.gui.views.AbstractFragment;
+import com.frostwire.util.Logger;
+
+/**
+ * @author gubatron
+ * @author aldenml
+ * @author marcelinkaaa
+ */
+public final class TransferDetailFragment extends AbstractFragment {
+
+    private static final Logger LOG = Logger.getLogger(TransferDetailFragment.class);
+
+    public TransferDetailFragment() {
+        super(R.layout.fragment_transfer_detail);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_transfer_detail_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+}
