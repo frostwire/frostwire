962941a7d046cb2ead4e36880cb659e4e3ad93b2
gubatron <gubatron@gmail.com>
2017-09-22 20:35:18 -0600

    [desktop] 6.5.8 build 245

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -43,7 +43,7 @@ public final class FrostWireUtils {
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 244;
+    private static final int BUILD_NUMBER = 245;
 
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
