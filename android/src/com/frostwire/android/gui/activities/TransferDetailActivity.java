871931b58b70428ada649b11a917db846fbbb28c
gubatron <gubatron@gmail.com>
2017-10-10 14:55:10 -0600

    [android] Transfer detail fragments reused. cleanup on fragment classes

--- android/src/com/frostwire/android/gui/activities/TransferDetailActivity.java
+++ android/src/com/frostwire/android/gui/activities/TransferDetailActivity.java
@@ -1,24 +1,39 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml), Marcelina Knitter (@marcelinkaaa)
+ * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.frostwire.android.gui.activities;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v13.app.FragmentPagerAdapter;
-import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.frostwire.android.R;
 import com.frostwire.android.gui.fragments.TransferDetailDetailsFragment;
 import com.frostwire.android.gui.fragments.TransferDetailFilesFragment;
 import com.frostwire.android.gui.fragments.TransferDetailPeersFragment;
 import com.frostwire.android.gui.fragments.TransferDetailPiecesFragment;
-import com.frostwire.android.gui.fragments.TransferDetailTrackersFragment;
 import com.frostwire.android.gui.fragments.TransferDetailStatusFragment;
+import com.frostwire.android.gui.fragments.TransferDetailTrackersFragment;
 import com.frostwire.android.gui.views.AbstractActivity;
 import com.frostwire.android.gui.views.AbstractFragment;
+
+import java.util.HashMap;
 
 public class TransferDetailActivity extends AbstractActivity {
 
@@ -40,70 +55,82 @@ public class TransferDetailActivity extends AbstractActivity {
         tabLayout.setupWithViewPager(mViewPager);
     }
 
-    public static class PlaceholderFragment extends Fragment {
-
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_transfer_detail_label, container, false);
-        }
-    }
 
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
 
+        private final HashMap<Integer, AbstractFragment> detailFragments;
+        private final String[] titles;
+
         SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
+            detailFragments = new HashMap<>();
+            titles = initPageTitles();
         }
 
         @Override
         public AbstractFragment getItem(int position) {
             switch(position) {
-                case 0: return TransferDetailDetailsFragment.newInstance(getString(R.string.details).toUpperCase());
-                case 1: return TransferDetailStatusFragment.newInstance(getString(R.string.status).toUpperCase());
-                case 2: return TransferDetailFilesFragment.newInstance(getString(R.string.files).toUpperCase());
-                case 3: return TransferDetailTrackersFragment.newInstance(getString(R.string.trackers).toUpperCase());
-                case 4: return TransferDetailPeersFragment.newInstance(getString(R.string.peers).toUpperCase());
-                case 5: return TransferDetailPiecesFragment.newInstance(getString(R.string.pieces).toUpperCase());
-                default: return TransferDetailStatusFragment.newInstance(getString(R.string.status).toUpperCase());
+                case 0:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_details)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_details, new TransferDetailDetailsFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_details);
+                case 1:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_status)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_status, new TransferDetailStatusFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_status);
+                case 2:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_files)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_files, new TransferDetailFilesFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_files);
+                case 3:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_trackers)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_trackers, new  TransferDetailTrackersFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_trackers);
+                case 4:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_peers)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_peers, new TransferDetailPeersFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_peers);
+                case 5:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_pieces)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_pieces, new TransferDetailPiecesFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_pieces);
+                default:
+                    if (!detailFragments.containsKey(R.layout.fragment_transfer_detail_status)) {
+                        detailFragments.put(R.layout.fragment_transfer_detail_status, new TransferDetailStatusFragment());
+                    }
+                    return detailFragments.get(R.layout.fragment_transfer_detail_status);
             }
         }
 
         @Override
         public int getCount() {
-            return 6;
+            return titles.length;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return getString(R.string.details).toUpperCase();
-                case 1:
-                    return getString(R.string.status).toUpperCase();
-                case 2:
-                    return getString(R.string.files).toUpperCase();
-                case 3:
-                    return getString(R.string.trackers).toUpperCase();
-                case 4:
-                    return getString(R.string.peers).toUpperCase();
-                case 5:
-                    return getString(R.string.pieces).toUpperCase();
-                default: return getString(R.string.status).toUpperCase();
+            return titles[position];
+        }
+
+        private String[] initPageTitles() {
+            String[] pageTitles = new String[] {
+                    getString(R.string.details),  // 0
+                    getString(R.string.status),   // 1
+                    getString(R.string.trackers), // 2
+                    getString(R.string.peers),    // 3
+                    getString(R.string.pieces),   // 4
+                    getString(R.string.status),   // 5
+            };
+            for (int i=0; i < pageTitles.length; i++) {
+                pageTitles[i] = pageTitles[i].toUpperCase();
             }
+            return pageTitles;
         }
     }
 }
