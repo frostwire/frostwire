a634bf4983af8394df49c228924d6d597045711e
gubatron <gubatron@gmail.com>
2017-10-10 14:55:10 -0600

    [android] Transfer detail fragments reused. cleanup on fragment classes

--- android/src/com/frostwire/android/gui/fragments/TransferDetailFilesFragment.java
+++ android/src/com/frostwire/android/gui/fragments/TransferDetailFilesFragment.java
@@ -18,7 +18,6 @@
 
 package com.frostwire.android.gui.fragments;
 
-import android.os.Bundle;
 import com.frostwire.android.R;
 import com.frostwire.android.gui.views.AbstractFragment;
 
@@ -33,16 +32,5 @@ public class TransferDetailFilesFragment extends AbstractFragment {
     public TransferDetailFilesFragment() {
         super(R.layout.fragment_transfer_detail_files);
         setHasOptionsMenu(true);
-    }
-
-    public static TransferDetailFilesFragment newInstance(String text) {
-
-        TransferDetailFilesFragment f = new TransferDetailFilesFragment();
-        Bundle b = new Bundle();
-        b.putString("msg", text);
-
-        f.setArguments(b);
-
-        return f;
     }
 }
