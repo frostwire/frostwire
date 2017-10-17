13a7553640e3ec10bd883c02e2616c314a7d0409
gubatron <gubatron@gmail.com>
2017-10-16 23:26:48 -0600

    [android] Transfer Details, Status tab done

--- android/src/com/frostwire/android/gui/fragments/TransferDetailStatusFragment.java
+++ android/src/com/frostwire/android/gui/fragments/TransferDetailStatusFragment.java
@@ -24,6 +24,9 @@ import android.widget.TextView;
 
 import com.frostwire.android.R;
 import com.frostwire.android.gui.views.AbstractTransferDetailFragment;
+import com.frostwire.transfers.TransferState;
+
+import static com.frostwire.android.gui.util.UIUtils.getBytesInHuman;
 
 /**
  * @author gubatron
@@ -35,6 +38,13 @@ public class TransferDetailStatusFragment extends AbstractTransferDetailFragment
 
     private TextView completedTextView;
     private TextView timeLeftTextView;
+    private TextView downloadedTextView;
+    private TextView uploadedTextView;
+    private TextView shareRatioTextView;
+    private TextView peersTextView;
+    private TextView seedsTextView;
+    private TextView activeTimeTextView;
+    private TextView seedingTimeTextView;
 
     public TransferDetailStatusFragment() {
         super(R.layout.fragment_transfer_detail_status);
@@ -47,12 +57,43 @@ public class TransferDetailStatusFragment extends AbstractTransferDetailFragment
         completedTextView.setText("");
         timeLeftTextView = findView(v, R.id.fragment_transfer_detail_status_time_left);
         timeLeftTextView.setText("");
+        downloadedTextView = findView(v, R.id.fragment_transfer_detail_status_downloaded);
+        downloadedTextView.setText("");
+        uploadedTextView = findView(v, R.id.fragment_transfer_detail_status_uploaded);
+        uploadedTextView.setText("");
+        shareRatioTextView = findView(v, R.id.fragment_transfer_detail_status_share_ratio);
+        shareRatioTextView.setText("");
+        peersTextView = findView(v, R.id.fragment_transfer_detail_status_peers);
+        peersTextView.setText("");
+        seedsTextView = findView(v, R.id.fragment_transfer_detail_status_seeds);
+        seedsTextView.setText("");
+        activeTimeTextView = findView(v, R.id.fragment_transfer_detail_status_active_time);
+        activeTimeTextView.setText("");
+        seedingTimeTextView = findView(v, R.id.fragment_transfer_detail_status_seeding_time);
+        seedingTimeTextView.setText("");
     }
 
     @Override
     public void onTime() {
         super.onTime();
         completedTextView.setText(uiBittorrentDownload.getProgress() + "%");
-        timeLeftTextView.setText(seconds2time(uiBittorrentDownload.getETA()));
+        if (uiBittorrentDownload.getState() == TransferState.DOWNLOADING) {
+            timeLeftTextView.setText(seconds2time(uiBittorrentDownload.getETA()));
+        } else {
+            timeLeftTextView.setText("");
+        }
+
+        downloadedTextView.setText(getString(R.string.m_of_n_strings,
+                getBytesInHuman(uiBittorrentDownload.getDl().getTotalBytesReceived()),
+                getBytesInHuman(uiBittorrentDownload.getSize())));
+
+        uploadedTextView.setText(getBytesInHuman(uiBittorrentDownload.getBytesSent()));
+        shareRatioTextView.setText(getShareRatio(uiBittorrentDownload));
+        peersTextView.setText(getString(R.string.m_of_n_decimals, uiBittorrentDownload.getConnectedPeers(), uiBittorrentDownload.getTotalPeers()));
+        seedsTextView.setText(getString(R.string.m_of_n_decimals, uiBittorrentDownload.getConnectedSeeds(), uiBittorrentDownload.getTotalSeeds()));
+        if (torrentHandle != null) {
+            activeTimeTextView.setText(seconds2time(torrentHandle.status().activeDuration()));
+            seedingTimeTextView.setText(seconds2time(torrentHandle.status().seedingDuration()));
+        }
     }
 }
