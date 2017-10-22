3f0b22099939a52f1ef5c4e6ef1763012dbf669a
gubatron <gubatron@gmail.com>
2017-10-22 14:38:22 -0600

    [android] progress on Transfers Details > Trackers tab

--- android/src/com/frostwire/android/gui/fragments/TransferDetailTrackersFragment.java
+++ android/src/com/frostwire/android/gui/fragments/TransferDetailTrackersFragment.java
@@ -19,10 +19,26 @@
 package com.frostwire.android.gui.fragments;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.frostwire.android.R;
+import com.frostwire.android.gui.services.Engine;
+import com.frostwire.android.gui.transfers.UIBittorrentDownload;
 import com.frostwire.android.gui.views.AbstractTransferDetailFragment;
+import com.frostwire.bittorrent.BTEngine;
+import com.frostwire.jlibtorrent.AnnounceEntry;
+import com.frostwire.jlibtorrent.TorrentHandle;
+import com.frostwire.util.Ref;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 /**
  * @author gubatron
@@ -35,10 +51,164 @@ public class TransferDetailTrackersFragment extends AbstractTransferDetailFragme
         super(R.layout.fragment_transfer_detail_trackers);
     }
 
+    private TextView dhtStatus;
+    private TextView pexStatus;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button addTrackerButton;
+
+    private TrackerRecyclerViewAdapter adapter;
+
     @Override
     protected void initComponents(View v, Bundle savedInstanceState) {
         super.initComponents(v, savedInstanceState);
-        //TODO: open a dialog to add a tracker
-        //Button addTrackerButton = findView(v, R.id.fragment_transfer_detail_trackers_add_tracker_button);
+
+        dhtStatus = findView(v, R.id.fragment_transfer_detail_trackers_dht_status);
+        pexStatus = findView(v, R.id.fragment_transfer_detail_trackers_pex_status);
+
+        recyclerView = findView(v, R.id.fragment_transfer_detail_trackers_address_recyclerview);
+
+        addTrackerButton = findView(v, R.id.fragment_transfer_detail_trackers_add_tracker_button);
+    }
+
+    @Override
+    public void onTime() {
+        super.onTime();
+
+        if (adapter == null) {
+            layoutManager = new LinearLayoutManager(getActivity());
+            adapter = new TrackerRecyclerViewAdapter(uiBittorrentDownload);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(adapter);
+        } else {
+
+        }
+        dhtStatus.setText(BTEngine.getInstance().isDhtRunning() ? R.string.working : R.string.disabled);
+        pexStatus.setText("--");
+    }
+
+    private static final class AddTrackerButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private static final class TrackerItemViewHolder extends RecyclerView.ViewHolder {
+
+        private final WeakReference<TrackerRecyclerViewAdapter> adapterRef; // so we can notify it when we've changed its underlying data
+        private final TextView trackerTextView;
+        private final ImageButton editButton;
+        private final ImageButton removeButton;
+        private final TorrentHandle torrentHandle;
+        private int trackerOffset;
+
+        public TrackerItemViewHolder(final View itemView,
+                                     final TrackerRecyclerViewAdapter adapter,
+                                     final TorrentHandle torrentHandle,
+                                     int trackerOffset) {
+            super(itemView);
+            adapterRef = Ref.weak(adapter);
+            trackerTextView = itemView.findViewById(R.id.view_transfer_detail_tracker_address);
+            editButton = itemView.findViewById(R.id.view_transfer_detail_tracker_edit_button);
+            removeButton = itemView.findViewById(R.id.view_transfer_detail_tracker_remove_button);
+
+            this.torrentHandle = torrentHandle;
+            this.trackerOffset = trackerOffset;
+
+            editButton.setOnClickListener(new OnEditTrackerClicked(this));
+            removeButton.setOnClickListener(new OnRemoveTrackerClicked(this));
+        }
+
+        public void updateData(int trackerOffset) {
+            List<AnnounceEntry> trackers = torrentHandle.trackers();
+            if (trackers == null || trackers.isEmpty() || trackerOffset >= trackers.size()) {
+                return;
+            }
+            AnnounceEntry trackerEntry = trackers.get(trackerOffset);
+            trackerTextView.setText(trackerEntry.url());
+        }
+
+        private static final class OnEditTrackerClicked implements View.OnClickListener {
+
+            private final WeakReference<TrackerItemViewHolder> vhRef;
+
+            OnEditTrackerClicked(TrackerItemViewHolder vh) {
+                vhRef = Ref.weak(vh);
+            }
+
+            @Override
+            public void onClick(View v) {
+                if (!Ref.alive(vhRef)) {
+                    return;
+                }
+                // TODO: some edit textview dialog, once it's done this happens
+                // with the dialog's textview's contents.
+
+                TrackerItemViewHolder viewHolder = vhRef.get();
+                int trackerOffset = viewHolder.trackerOffset;
+                List<AnnounceEntry> trackers = viewHolder.torrentHandle.trackers();
+
+                // replace the entry
+                //AnnounceEntry entry = new AnnounceEntry(new announce_entry("udp://sometracker.com:8090"));
+
+                if (Ref.alive(viewHolder.adapterRef)) {
+                    viewHolder.adapterRef.get().notifyDataSetChanged();
+                }
+            }
+        }
+
+        private static final class OnRemoveTrackerClicked implements View.OnClickListener {
+            private final WeakReference<TrackerItemViewHolder> vhRef;
+
+            OnRemoveTrackerClicked(TrackerItemViewHolder vh) {
+                vhRef = Ref.weak(vh);
+            }
+
+            @Override
+            public void onClick(View v) {
+                if (!Ref.alive(vhRef)) {
+                    return;
+                }
+                // TODO: are you sure you want to remove X dialog or toast would be nice
+                int trackerOffset = vhRef.get().trackerOffset;
+                List<AnnounceEntry> trackers = vhRef.get().torrentHandle.trackers();
+
+                TrackerItemViewHolder viewHolder = vhRef.get();
+                if (Ref.alive(viewHolder.adapterRef)) {
+                    viewHolder.adapterRef.get().notifyDataSetChanged();
+                }
+            }
+        }
+    }
+
+    private static final class TrackerRecyclerViewAdapter extends RecyclerView.Adapter<TrackerItemViewHolder> {
+        private final UIBittorrentDownload uiBittorrentDownload;
+
+        public TrackerRecyclerViewAdapter(final UIBittorrentDownload uiBittorrentDownload) {
+            this.uiBittorrentDownload = uiBittorrentDownload;
+        }
+
+        @Override
+        public TrackerItemViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+            View inflatedView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_transfer_detail_tracker_item, parent, false);
+            return new TrackerItemViewHolder(inflatedView, this, uiBittorrentDownload.getDl().getTorrentHandle(), i);
+        }
+
+        @Override
+        public void onBindViewHolder(TrackerItemViewHolder trackerItemViewHolder, int i) {
+            trackerItemViewHolder.updateData(i);
+        }
+
+        @Override
+        public int getItemCount() {
+            if (uiBittorrentDownload == null ||
+                uiBittorrentDownload.getDl() == null ||
+                uiBittorrentDownload.getDl().getTorrentHandle() == null) {
+                return 0;
+            }
+            TorrentHandle torrentHandle = uiBittorrentDownload.getDl().getTorrentHandle();
+            return torrentHandle.trackers() == null ? 0 : torrentHandle.trackers().size();
+        }
     }
 }
