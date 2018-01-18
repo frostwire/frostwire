1c332a0df74319ad26448476e472fb8cf6fd6336
gubatron <gubatron@gmail.com>
2018-01-17 19:07:50 -0700

    [desktop] 6.6.1, build 250. messagebundles .po files updated after re-extraction

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,12 +38,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.6.1";
+    private static final String FROSTWIRE_VERSION = "6.6.2";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 249;
+    private static final int BUILD_NUMBER = 250;
 
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
