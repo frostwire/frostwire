d86e84474463b3cc52d76ab80bbf8c5b618b4a5a
gubatron <gubatron@gmail.com>
2018-02-01 23:21:28 -0700

    [desktop] minor interface cleanup

--- desktop/src/com/frostwire/gui/mplayer/MPlayerComponent.java
+++ desktop/src/com/frostwire/gui/mplayer/MPlayerComponent.java
@@ -1,6 +1,6 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml), Erich Pleny (erichpleny)
- * Copyright (c) 2011-2014, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2018, FrostWire(R). All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,8 +28,8 @@ import java.awt.*;
  */
 public interface MPlayerComponent {
 
-	public Component getComponent();
-	public boolean toggleFullScreen();
+	Component getComponent();
+	boolean toggleFullScreen();
 	
-	public long getWindowID();
+	long getWindowID();
 }
