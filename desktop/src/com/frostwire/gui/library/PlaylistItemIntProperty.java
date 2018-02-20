aa3a5dbb49c77cf97ac87e7b09caa3a9c3d4cf01
Alden Torres <aldenml@gmail.com>
2018-02-20 09:14:25 -0500

    [desktop] minor optimization in PlaylistItemIntProperty

--- desktop/src/com/frostwire/gui/library/PlaylistItemIntProperty.java
+++ desktop/src/com/frostwire/gui/library/PlaylistItemIntProperty.java
@@ -1,28 +1,25 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2014, 2013, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2018, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.gui.library;
 
 /**
- * 
  * @author gubatron
  * @author aldenml
- *
  */
 class PlaylistItemIntProperty extends PlaylistItemProperty<PlaylistItemIntProperty> {
 
@@ -43,7 +40,7 @@ class PlaylistItemIntProperty extends PlaylistItemProperty<PlaylistItemIntProper
 
     @Override
     public int compareTo(PlaylistItemIntProperty o) {
-        return Integer.valueOf(intValue).compareTo(o.intValue);
+        return Integer.compare(intValue, o.intValue);
     }
 
     @Override
