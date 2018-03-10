3f44a918e820c94cff7a740b00937185b35a518c
Alden Torres <aldenml@gmail.com>
2018-03-10 16:20:58 -0500

    [android] import cleanup in Debug class

--- android/src/com/frostwire/android/util/Debug.java
+++ android/src/com/frostwire/android/util/Debug.java
@@ -32,7 +32,6 @@ import com.frostwire.android.gui.services.Engine;
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
