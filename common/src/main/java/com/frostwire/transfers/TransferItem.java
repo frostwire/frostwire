7e8d13af81e27e30fbe4f7829f93ecd3a964ddd6
gubatron <gubatron@gmail.com>
2018-03-24 12:01:55 -0600

    [common] TransferItem interface cleanup

--- common/src/main/java/com/frostwire/transfers/TransferItem.java
+++ common/src/main/java/com/frostwire/transfers/TransferItem.java
@@ -1,19 +1,19 @@
 /*
- * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2014, FrostWire(R). All rights reserved.
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
+ *            Marcelina Knitter (@marcelinkaaa)
+ * Copyright (c) 2011-2018, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.transfers;
@@ -26,9 +26,9 @@ import java.io.File;
  */
 public interface TransferItem {
 
-    public String getName();
+    String getName();
 
-    public String getDisplayName();
+    String getDisplayName();
 
     /**
      * Actual file in the file system to which the data is saved. Ideally it should be
@@ -36,20 +36,20 @@ public interface TransferItem {
      *
      * @return
      */
-    public File getFile();
+    File getFile();
 
-    public long getSize();
+    long getSize();
 
-    public boolean isSkipped();
+    boolean isSkipped();
 
-    public long getDownloaded();
+    long getDownloaded();
 
     /**
      * [0..100]
      *
      * @return
      */
-    public int getProgress();
+    int getProgress();
 
-    public boolean isComplete();
+    boolean isComplete();
 }
