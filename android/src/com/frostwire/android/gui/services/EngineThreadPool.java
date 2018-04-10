76c13c2f65f08faa35f19b284d592a9dfd788993
Alden Torres <aldenml@gmail.com>
2018-04-10 17:43:41 -0400

    [android] dump thread pool information in case of queue overflow, only active in debug build

--- android/src/com/frostwire/android/gui/services/EngineThreadPool.java
+++ android/src/com/frostwire/android/gui/services/EngineThreadPool.java
@@ -20,6 +20,8 @@ package com.frostwire.android.gui.services;
 import com.frostwire.android.util.Debug;
 import com.frostwire.util.ThreadPool;
 
+import java.util.Map;
+import java.util.WeakHashMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -37,8 +39,14 @@ final class EngineThreadPool extends ThreadPool {
     // 17 in a medium hardware phone
     private static final int MAXIMUM_POOL_SIZE = 4;
 
+    private final WeakHashMap<Object, String> taskStack;
+    private final WeakHashMap<Thread, TaskInfo> taskInfo;
+
     EngineThreadPool() {
         super("Engine", MAXIMUM_POOL_SIZE, new LinkedBlockingQueue<>(), false);
+
+        taskStack = new WeakHashMap<>();
+        taskInfo = new WeakHashMap<>();
     }
 
     @Override
@@ -65,20 +73,79 @@ final class EngineThreadPool extends ThreadPool {
         return super.submit(task);
     }
 
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        if (Debug.isEnabled()) {
+            String stack = taskStack.get(r);
+            if (stack != null) {
+                taskInfo.put(t, new TaskInfo(System.nanoTime(), stack));
+            }
+        }
+
+        super.beforeExecute(t, r);
+    }
+
     private void verifyTask(Object task) {
         if (Debug.hasContext(task)) {
             throw new RuntimeException("Runnable/task contains context, possible context leak");
         }
 
+        if (Debug.isEnabled()) {
+            taskStack.put(task, getStack());
+        }
+
         // if debug/development, allow only 20 tasks in the queue
         if (Debug.isEnabled() && getQueue().size() > 20) {
-            // TODO: review this condition, since debugging session is not the same
-            // as "paused at a breakpoint"
-            if (android.os.Debug.isDebuggerConnected()) {
-                return;
-            }
-
+            dumpTasks();
             throw new RuntimeException("Too many tasks in the queue");
+        }
+    }
+
+    private void dumpTasks() {
+        System.out.println("Running threads in engine pool");
+        long now = System.nanoTime();
+        for (Map.Entry<Thread, TaskInfo> e : taskInfo.entrySet()) {
+            String threadName = e.getKey().getName();
+            if (threadName != null && threadName.contains("thread-idle")) {
+                continue;
+            }
+            System.out.println("Thread name: " + threadName);
+            System.out.println("\tTime running: " + ((now - e.getValue().time) / 1000000) + "ms");
+            System.out.println("\tStack trace:");
+            System.out.println(e.getValue().stack);
+        }
+    }
+
+    private static String getStack() {
+        StackTraceElement[] callStack = Thread.currentThread().getStackTrace();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 5; i < callStack.length; i++) {
+            StackTraceElement caller = callStack[i];
+            sb.append("\t\t at ");
+            sb.append(caller.getClassName()).append(".").append(caller.getMethodName());
+
+            String fileName = caller.getFileName();
+            int lineNumber = caller.getLineNumber();
+            if (fileName != null) {
+                sb.append("(").append(fileName);
+                if (lineNumber > 0) {
+                    sb.append(":").append(lineNumber);
+                }
+                sb.append(")");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static final class TaskInfo {
+
+        final long time;
+        final String stack;
+
+        TaskInfo(long time, String stack) {
+            this.time = time;
+            this.stack = stack;
         }
     }
 }
