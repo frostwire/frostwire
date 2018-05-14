4ae36d011f2d55789efd2a7ff01ebbf674c2065f
Jose Molina <votaguz@gmail.com>
2018-05-13 20:51:06 -0600

    [desktop] temp

--- desktop/src/com/limegroup/gnutella/gui/SendFeedbackDialog.java
+++ desktop/src/com/limegroup/gnutella/gui/SendFeedbackDialog.java
@@ -1,27 +1,33 @@
 package com.limegroup.gnutella.gui;
 
+import com.frostwire.util.HttpClientFactory;
+import com.frostwire.util.UserAgentGenerator;
+import com.frostwire.util.http.HttpClient;
 import com.limegroup.gnutella.gui.bugs.LocalClientInfo;
-import com.limegroup.gnutella.util.FrostWireUtils;
 import net.miginfocom.swing.MigLayout;
-import org.limewire.util.OSUtils;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Properties;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.util.HashMap;
 
 
 public class SendFeedbackDialog {
     private final JDialog DIALOG;
+    private final JTextField emailTextField;
+    private final JButton sendButton;
+    private final JButton cancelButton;
+    private JTextArea messageTextArea;
+    final String FEEDBACK_HINT = I18n.tr("How can we make FrostWire better? (Please make sure your firewall or antivirus is not blocking FrostWire)");
 
     SendFeedbackDialog() {
         DIALOG = new JDialog(GUIMediator.getAppFrame());
         DIALOG.setModal(true);
         DIALOG.setTitle(I18n.tr("Send Feedback"));
-        DIALOG.setMinimumSize(new Dimension(750, 400));
+        DIALOG.setMinimumSize(new Dimension(750, 350));
         DIALOG.setResizable(false);
         DIALOG.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         DIALOG.pack();
@@ -37,39 +43,53 @@ public class SendFeedbackDialog {
         JPanel feedbackPanel = new JPanel(new MigLayout("fill, insets 10 10"));
 
         // Message
-        JTextArea messageTextArea = new JTextArea(I18n.tr("How can we make FrostWire better?"));
-
-        messageTextArea.setRows(5);
-        messageTextArea.setSize(new Dimension(380, 300));
+        messageTextArea = new JTextArea(FEEDBACK_HINT);
+        messageTextArea.selectAll();
+        messageTextArea.setRows(7);
+        messageTextArea.setSize(new Dimension(380, 400));
         messageTextArea.setLineWrap(true);
         messageTextArea.setWrapStyleWord(true);
-
+        messageTextArea.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                onMessageTextAreaFirstClick();
+            }
+        });
         feedbackPanel.add(messageTextArea, "spanx 2, growx, wrap");
 
         // Optional email
-//        feedbackPanel.add(new JLabel(I18n.tr("Email (Optional)")), "spanx 2, wrap");
-        feedbackPanel.add(new JTextField(I18n.tr("Where we can contact you?")), "spanx 2, growx, wrap");
+        JPanel emailPanel = new JPanel(new MigLayout("fill, insets 0 0"));
+        JLabel emailLabel = new JLabel(I18n.tr("Email (Optional)"));
+        emailLabel.setEnabled(false);
+        emailPanel.add(emailLabel, "shrink, align left");
+        emailTextField = new JTextField();
+        emailPanel.add(emailTextField, "align left, pushx 1.0, growx, wrap");
+        feedbackPanel.add(emailPanel, "spanx 2, growx, wrap");
 
         // System Information
-        feedbackPanel.add(new JLabel(I18n.tr("System Information")),"spanx 2, wrap");
-        JTextArea systemInformationTextArea = new JTextArea(getSytemInformation());
-        systemInformationTextArea.setLineWrap(true);
-        systemInformationTextArea.setEditable(false);
+        JLabel systemInformationTextArea = new JLabel(getSytemInformation());
         systemInformationTextArea.setEnabled(false);
         systemInformationTextArea.setAutoscrolls(true);
+
         feedbackPanel.add(systemInformationTextArea, "spanx 2, grow, wrap");
 
 
         // Buttons
-        JButton sendButton = new JButton(I18n.tr("Send"));
+        sendButton = new JButton(I18n.tr("Send"));
         sendButton.addActionListener(this::onSendClicked);
         feedbackPanel.add(sendButton, "pushx 1.0, alignx right");
 
-        JButton cancelButton = new JButton(I18n.tr("Cancel"));
+        cancelButton = new JButton(I18n.tr("Cancel"));
         cancelButton.addActionListener(GUIUtils.getDisposeAction());
         feedbackPanel.add(cancelButton, "alignx right");
 
         baseContentPane.add(feedbackPanel, "grow, wrap");
+    }
+
+    private void onMessageTextAreaFirstClick() {
+        if (messageTextArea.getText().equals(FEEDBACK_HINT)) {
+            messageTextArea.setText("");
+        }
     }
 
     void showDialog() {
@@ -84,12 +104,29 @@ public class SendFeedbackDialog {
 
     private String getSytemInformation(){
         LocalClientInfo mock = new LocalClientInfo(new Throwable("mock"), "", "", false);
-        return mock.getBasicSystemInfo()[0].toString();
+        String basicSystemInfo = mock.getBasicSystemInfo().sw.toString();
+        String basicSystemInfoHtml = "<html>" + basicSystemInfo.replace("\n","<br/>") + "</html>";
+        return basicSystemInfoHtml;
     }
 
     @SuppressWarnings("unused")
     private void onSendClicked(ActionEvent e) {
-        System.out.println("On Send button clicked");
+        messageTextArea.setEnabled(false);
+        emailTextField.setEnabled(false);
+        sendButton.setText(I18n.tr("Sending..."));
+        sendButton.setEnabled(false);
+        cancelButton.setVisible(false);
+    }
+
+    private static void submitFeedbackAsync() {
+        HashMap<String, String> feedbackData = new HashMap<>();
+        HttpClient httpClient = HttpClientFactory.getInstance(HttpClientFactory.HttpContext.MISC);
+        try {
+            httpClient.post("http://www1.frostwire.com/desktop-feedback",10000, UserAgentGenerator.getUserAgent(), feedbackData);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
     }
 
 }
