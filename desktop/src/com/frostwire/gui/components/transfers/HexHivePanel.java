087f20950c76f0ee0b863a5f920577f5c71d1e0e
gubatron <gubatron@gmail.com>
2018-06-18 21:38:25 -0600

    [desktop] Compilation error on HexHivPanel, moved to com.frostwire.gui.components.transfers
    Added license also

--- desktop/src/com/frostwire/gui/components/transfers/HexHivePanel.java
+++ desktop/src/com/frostwire/gui/components/transfers/HexHivePanel.java
@@ -1,4 +1,23 @@
-package com.frostwire.gui.tabs;
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml),
+ * Marcelina Knitter (@marcelinkaaa), Jose Molina (@votaguz)
+ * Copyright (c) 2011-2018, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.frostwire.gui.components.transfers;
 
 import com.frostwire.util.Logger;
 import com.limegroup.gnutella.gui.GUIMediator;
@@ -10,7 +29,6 @@ import java.awt.event.ComponentEvent;
 import java.awt.geom.GeneralPath;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.ExecutorService;
-
 
 public class HexHivePanel extends JPanel {
     private static final Logger LOG = Logger.getLogger(HexHivePanel.class);
@@ -83,7 +101,7 @@ public class HexHivePanel extends JPanel {
         if (hexDataAdapter != null && hexDataAdapter.getFullHexagonsCount() >= 0 && canvasWidth > 0 && canvasHeight > 0) {
             threadPool.execute(() -> {
                 LOG.info("updateData() about to execute asyncDraw on thread");
-                BufferedImage backgroundBitmap = asyncDraw(canvasWidth, canvasHeight, hexDataAdapter);
+                BufferedImage backgroundBitmap = asyncDraw(hexDataAdapter);
                 synchronized (bitmapLock) {
                     bitmap = backgroundBitmap;
                 }
