33b22a387d49963a3233bf13a272a9598f1ae63b
gubatron <gubatron@gmail.com>
2018-06-28 07:24:36 -0600

    [desktop] preamble to pieces details

--- desktop/src/com/frostwire/gui/components/transfers/TransferDetailPieces.java
+++ desktop/src/com/frostwire/gui/components/transfers/TransferDetailPieces.java
@@ -18,19 +18,91 @@
 
 package com.frostwire.gui.components.transfers;
 
-import com.frostwire.gui.bittorrent.BTDownload;
 import com.frostwire.gui.bittorrent.BittorrentDownload;
+import com.frostwire.jlibtorrent.PieceIndexBitfield;
+import com.frostwire.jlibtorrent.TorrentHandle;
+import com.frostwire.jlibtorrent.TorrentInfo;
+import com.frostwire.jlibtorrent.TorrentStatus;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 
 public final class TransferDetailPieces extends JPanel implements TransferDetailComponent.TransferDetailPanel {
-    public TransferDetailPieces() {
+    private HexHivePanel HEXHIVEPANEL;
+    private HexHivePanel.HexDataAdapter hexPanelAdapter;
+
+    TransferDetailPieces() {
         super();
-        add(new JLabel("PIECES HERE"));
+        HEXHIVEPANEL = new HexHivePanel(0x264053, 0xf2f2f2, 0x33b5e5,0xf2f2f2, 0, 0, 0, 0);
+        setLayout(new MigLayout("fillx, insets 0 0 0 0"));
+        add(HEXHIVEPANEL);
     }
 
     @Override
     public void updateData(BittorrentDownload btDownload) {
+        HEXHIVEPANEL.updateData(hexPanelAdapter);
+    }
 
+    private static class HexHiveAdapter implements HexHivePanel.HexDataAdapter<BittorrentDownload> {
+        private BittorrentDownload bittorrentDownload;
+        @Override
+        public void updateData(BittorrentDownload data) {
+            /*
+            bittorrentDownload = data;
+            TorrentStatus status = torrentHandle.status(TorrentHandle.QUERY_PIECES);
+            TorrentInfo torrentInfo = torrentHandle.torrentFile();
+
+            if (pieceSizeString == null) {
+                pieceSizeString = UIUtils.getBytesInHuman(torrentInfo.pieceSize(0));
+            }
+
+            if (totalPieces == -1) {
+                totalPieces = torrentInfo.numPieces();
+                piecesNumberTextView.setText(String.valueOf(totalPieces));
+                //progressBar.setVisibility(View.VISIBLE);
+                hexHiveView.setVisibility(View.GONE);
+            }
+
+            PieceIndexBitfield pieces = status.pieces();
+            long piecesCount = pieces.count();
+            if (isAdded()) {
+                // I do this color look-up only once and pass it down to the view holder
+                // otherwise it has to be done thousands of times.
+                pieceSizeTextView.setText(pieceSizeString);
+                hexDataAdapter = new PieceAdapter(totalPieces, pieces);
+                hexHiveView.setVisibility(View.VISIBLE);
+            }
+            if (hexDataAdapter != null) {
+                if (piecesCount >= 0) {
+                    hexDataAdapter.updateData(pieces);
+                }
+                //noinspection unchecked
+                hexHiveView.updateData(hexDataAdapter);
+                piecesNumberTextView.setText(piecesCount + "/" + totalPieces);
+            }
+            */
+        }
+
+        @Override
+        public int getTotalHexagonsCount() {
+            if (bittorrentDownload == null) {
+                return 0;
+            }
+            //bittorrentDownload.getDl().
+            return 0;
+        }
+
+        @Override
+        public int getFullHexagonsCount() {
+            if (bittorrentDownload == null) {
+                return 0;
+            }
+            return 0;
+        }
+
+        @Override
+        public boolean isFull(int hexOffset) {
+            return false;
+        }
     }
 }
