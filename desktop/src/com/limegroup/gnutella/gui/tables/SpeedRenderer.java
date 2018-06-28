bf631a04f73d78ca4cf041c1621c714d7381f843
gubatron <gubatron@gmail.com>
2018-06-28 05:27:48 -0600

    [desktop] no warnings on SpeedRenderer

--- desktop/src/com/limegroup/gnutella/gui/tables/SpeedRenderer.java
+++ desktop/src/com/limegroup/gnutella/gui/tables/SpeedRenderer.java
@@ -9,35 +9,37 @@ import java.awt.*;
  * Simple renderer for speeds.
  */
 public final class SpeedRenderer extends DefaultTableBevelledCellRenderer {
-	
-	/**
-     * 
+
+    /**
+     *
      */
     private static final long serialVersionUID = -3181771818657704593L;
 
     /**
-	 * Constructs the speed drawer.
-	 */
-	public SpeedRenderer() {
-	    super();
-	}
+     * Constructs the speed drawer.
+     */
+    SpeedRenderer() {
+        super();
+    }
 
-	/**
-	 * Draws a speed.
-	 */
-	public Component getTableCellRendererComponent
-		(JTable table,Object value,boolean isSelected,
-		 boolean hasFocus,int row,int column) {
-		    
-        Double d = (Double)value;
+    /**
+     * Draws a speed.
+     */
+    public Component getTableCellRendererComponent(JTable table,
+                                                   Object value,
+                                                   boolean isSelected,
+                                                   boolean hasFocus,
+                                                   int row,
+                                                   int column) {
+        Double d = (Double) value;
         String s = "";
-        if(d != null) {
-            double dd = d.doubleValue();
-            if( dd != -1 )
+        if (d != null) {
+            double dd = d;
+            if (dd != -1) {
                 s = GUIUtils.rate2speed(dd);
+            }
         }
-
         return super.getTableCellRendererComponent(
-            table, s, isSelected, hasFocus, row, column);
-	}
+                table, s, isSelected, hasFocus, row, column);
+    }
 }
