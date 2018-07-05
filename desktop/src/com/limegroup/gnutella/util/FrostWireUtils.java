5f12d9ac5f6bea4033caa10c4472749f72da427f
gubatron <gubatron@gmail.com>
2018-07-04 22:24:01 -0600

    [desktop] New FrostWire 6.7.0 build 261. changelog

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -40,12 +40,12 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.6.8";
+    private static final String FROSTWIRE_VERSION = "6.7.0";
 
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 260;
+    private static final int BUILD_NUMBER = 261;
 
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
