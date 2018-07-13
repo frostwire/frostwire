4a4266eb04ff1f807d5373b64a052ec7075599c9
gubatron <gubatron@gmail.com>
2018-07-13 07:45:25 -0600

    [desktop] repaint total pieces label right away

--- desktop/src/com/frostwire/gui/components/transfers/TransferDetailPieces.java
+++ desktop/src/com/frostwire/gui/components/transfers/TransferDetailPieces.java
@@ -96,6 +96,7 @@ public final class TransferDetailPieces extends JPanel implements TransferDetail
 
     private void updateTotalPiecesLabel(String totalPieces) {
         totalPiecesLabel.setText("<html><b>" + I18n.tr("Total Pieces") + "</b>:" + totalPieces + "</html>");
+        totalPiecesLabel.paintImmediately(totalPiecesLabel.getVisibleRect());
     }
 
     ////////////////////////////////////////////////////////////////////////////////
