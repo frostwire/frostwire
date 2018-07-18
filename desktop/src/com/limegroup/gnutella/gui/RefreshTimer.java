85d1e14d5e51b24796ab7b0fcd4e257c13623ff6
Alden Torres <aldenml@gmail.com>
2018-07-18 14:47:52 -0400

    [desktop] code formatting in RefreshTimer

--- desktop/src/com/limegroup/gnutella/gui/RefreshTimer.java
+++ desktop/src/com/limegroup/gnutella/gui/RefreshTimer.java
@@ -5,45 +5,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * This class handles the timer that refreshes the gui after every 
+ * This class handles the timer that refreshes the gui after every
  * specified interval.
  */
 public final class RefreshTimer {
 
-	/** 
-	 * The interval between statistics updates in milliseconds. 
-	 */
-	private final int UPDATE_TIME = 1000;
+    /**
+     * The interval between statistics updates in milliseconds.
+     */
+    private final int UPDATE_TIME = 1000;
 
-	/**
-	 * variable for timer that updates the gui.
-	 */
-	private Timer _timer;
-	  
-	/**
-	 * Creates the timer and the ActionListener associated with it.
-	 */
-	public RefreshTimer() {
-		ActionListener refreshGUI = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+    /**
+     * variable for timer that updates the gui.
+     */
+    private Timer _timer;
+
+    /**
+     * Creates the timer and the ActionListener associated with it.
+     */
+    public RefreshTimer() {
+        ActionListener refreshGUI = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 refreshGUI();
-			}
-		};
-		
-		_timer = new Timer(UPDATE_TIME, refreshGUI);
-	}
+            }
+        };
 
-	/**
-	 * Starts the timer that updates the gui.
-	 */
-	public void startTimer() {
-		_timer.start();
-	}
+        _timer = new Timer(UPDATE_TIME, refreshGUI);
+    }
 
-	/** 
-	 * Refreshes all of the gui elements.
-	 */
-	private void refreshGUI() {
-		GUIMediator.instance().refreshGUI();
-	}
+    /**
+     * Starts the timer that updates the gui.
+     */
+    public void startTimer() {
+        _timer.start();
+    }
+
+    /**
+     * Refreshes all of the gui elements.
+     */
+    private void refreshGUI() {
+        GUIMediator.instance().refreshGUI();
+    }
 }
