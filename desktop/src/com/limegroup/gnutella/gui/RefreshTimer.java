b8b23edbd2159037a86fd1ab743de0a89a775a8e
Alden Torres <aldenml@gmail.com>
2018-07-18 14:52:58 -0400

    [desktop] trivial code refactor in RefreshTimer

--- desktop/src/com/limegroup/gnutella/gui/RefreshTimer.java
+++ desktop/src/com/limegroup/gnutella/gui/RefreshTimer.java
@@ -1,43 +1,35 @@
 package com.limegroup.gnutella.gui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * This class handles the timer that refreshes the gui after every
  * specified interval.
  */
-public final class RefreshTimer {
+final class RefreshTimer {
 
     /**
      * The interval between statistics updates in milliseconds.
      */
-    private final int UPDATE_TIME = 1000;
+    private static final int UPDATE_TIME = 1000;
 
     /**
      * variable for timer that updates the gui.
      */
-    private Timer _timer;
+    private Timer timer;
 
     /**
      * Creates the timer and the ActionListener associated with it.
      */
-    public RefreshTimer() {
-        ActionListener refreshGUI = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                refreshGUI();
-            }
-        };
-
-        _timer = new Timer(UPDATE_TIME, refreshGUI);
+    RefreshTimer() {
+        timer = new Timer(UPDATE_TIME, e -> refreshGUI());
     }
 
     /**
      * Starts the timer that updates the gui.
      */
-    public void startTimer() {
-        _timer.start();
+    void startTimer() {
+        timer.start();
     }
 
     /**
