20c2968f555315a1c97dd6530199aea634541a04
gubatron <gubatron@gmail.com>
2018-08-27 14:16:17 -0600

    [android] fixes issue where transfers wouldn't appear right away
    The old adapter instance kept coming back with 0 transfers for about 30 seconds. On destroy let's make sure the adapter reference is removed, next time the fragment is created a new adapter is created and it works right away when restoring a session from exit.
    
    perhaps the adapter should have a way to clear its inner lists  adapter.onDestroy() ?

--- android/src/com/frostwire/android/gui/fragments/TransfersFragment.java
+++ android/src/com/frostwire/android/gui/fragments/TransfersFragment.java
@@ -239,6 +239,7 @@ public class TransfersFragment extends AbstractFragment implements TimerObserver
     public void onDestroyView() {
         super.onDestroyView();
         subscription.unsubscribe();
+        adapter = null;
     }
 
     @Override
