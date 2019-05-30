751da38fc2c75655a11c7ce03de183cafb8212df
gubatron <gubatron@gmail.com>
2019-05-29 22:56:16 -0600

    [desktop] windows: 196k thread stack

--- desktop/run.bat
+++ desktop/run.bat
@@ -5,4 +5,4 @@ SET PATH=%PATH%;%LIBPATH%
 SET CLASSPATH=build\libs\frostwire.jar
 
 REM -Dsun.java2d.opengl=true
-java -Xms32m -Xmx512m -Xss128k -Ddebug=1 -Dcom.sun.management.jmxremote.port=9595 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=127.0.0.1 -Djava.library.path=%LIBPATH% com.limegroup.gnutella.gui.Main
+java -Xms32m -Xmx512m -Xss196k -Ddebug=1 -Dcom.sun.management.jmxremote.port=9595 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=127.0.0.1 -Djava.library.path=%LIBPATH% com.limegroup.gnutella.gui.Main
