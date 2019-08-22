c423a32aa593d742fb5f26b7a12577315edc5b5d
frostwire <contact@frostwire.com>
2019-08-22 17:13:21 -0600

    [desktop/macos] build.sh refactor and cleanup

--- desktop/lib/native-src/osx/build.sh
+++ desktop/lib/native-src/osx/build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 INCLUDE_PATH="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin"
 FLAGS="-fPIC -mmacosx-version-min=10.11 -fno-strict-aliasing -fvisibility=default -D_DARWIN_C_SOURCE -Os -arch x86_64 -dynamiclib"
+OUTPUT_PATH="../../native"
 
 echo "Building libSysteUtilities.dylib..."
-cc ${INCLUDE_PATH} ${FLAGS} -o ../../native/libSystemUtilities.dylib SystemUtilities.m
+cc ${INCLUDE_PATH} ${FLAGS} -o ${OUTPUT_PATH}/libSystemUtilities.dylib SystemUtilities.m
 
 echo "Building libMacOSXUtil.dylib..."
-cc ${INCLUDE_PATH} ${FLAGS} -framework Foundation -framework CoreServices -framework CoreFoundation -o ../../native/libMacOSXUtils.dylib MacOSXUtils.m
+cc ${INCLUDE_PATH} ${FLAGS} -framework Foundation -framework CoreServices -framework CoreFoundation -o ${OUTPUT_PATH}/libMacOSXUtils.dylib MacOSXUtils.m
 
 echo "Building libGURL.dylib..."
-cc ${INCLUDE_PATH} ${FLAGS} -framework Carbon -o ../../native/libGURL.dylib GURLjnilib.c
+cc ${INCLUDE_PATH} ${FLAGS} -framework Carbon -o ${OUTPUT_PATH}/libGURL.dylib GURLjnilib.c
 
-if [ -f a.out ]; then
-    rm -f a.out
-fi
+git status ${OUTPUT_PATH}
