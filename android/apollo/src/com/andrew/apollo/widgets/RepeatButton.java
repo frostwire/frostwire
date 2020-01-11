b1c1426e3c77458ee4ad162e68a15e1449b5cc1c
frostwire <contact@frostwire.com>
2020-01-11 12:00:56 -0700

    [android] RepeatButton async UI refresh optimizations

--- android/apollo/src/com/andrew/apollo/widgets/RepeatButton.java
+++ android/apollo/src/com/andrew/apollo/widgets/RepeatButton.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2012 Andrew Neal
  * Modified by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2013-2018, FrostWire(R). All rights reserved.
+ * Copyright (c) 2013-2020, FrostWire(R). All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,22 +18,33 @@
 
 package com.andrew.apollo.widgets;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
+import androidx.appcompat.widget.AppCompatImageButton;
+
 import com.andrew.apollo.MusicPlaybackService;
 import com.andrew.apollo.utils.MusicUtils;
 import com.frostwire.android.R;
+import com.frostwire.android.gui.services.Engine;
+import com.frostwire.android.util.Asyncs;
+import com.frostwire.util.Ref;
+
+import java.lang.ref.WeakReference;
+
+import static com.frostwire.android.util.Asyncs.async;
 
 /**
  * A custom {@link ImageButton} that represents the "repeat" button.
  *
  * @author Andrew Neal (andrewdneal@gmail.com)
+ * @author Angel Leon (@gubatron)
  */
-public final class RepeatButton extends ImageButton
+public final class RepeatButton extends AppCompatImageButton
         implements OnClickListener {
 
     private Runnable onClickedCallback;
@@ -55,7 +66,7 @@ public final class RepeatButton extends ImageButton
 
     @Override
     public void onClick(final View v) {
-        MusicUtils.cycleRepeat();
+        async(MusicUtils::cycleRepeat);
         updateRepeatState();
         if (onClickedCallback != null) {
             try {
@@ -69,7 +80,10 @@ public final class RepeatButton extends ImageButton
      * Sets the correct drawable for the repeat state.
      */
     public void updateRepeatState() {
-        int repeatMode = MusicUtils.getRepeatMode();
+        Asyncs.async(MusicUtils::getRepeatMode, RepeatButton::getRepeatModePost, this);
+    }
+
+    private void getRepeatModePost(Integer repeatMode) {
         switch (repeatMode) {
             case MusicPlaybackService.REPEAT_ALL:
                 setContentDescription(getResources().getString(R.string.accessibility_repeat_all));
