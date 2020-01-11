5ae5aa7075bcd3499c4d59cc4ffbb5849d77078c
frostwire <contact@frostwire.com>
2020-01-11 16:48:19 -0700

    [android] bye ApolloUtils.execute, consolidating all async execution to EngineThreadPool only

--- android/apollo/src/com/andrew/apollo/utils/ApolloUtils.java
+++ android/apollo/src/com/andrew/apollo/utils/ApolloUtils.java
@@ -17,20 +17,20 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
-import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.Toast;
+
 import com.andrew.apollo.Config;
 import com.andrew.apollo.cache.ImageCache;
 import com.andrew.apollo.cache.ImageFetcher;
-import com.frostwire.android.gui.services.Engine;
 import com.andrew.apollo.ui.activities.ShortcutActivity;
 import com.devspark.appmsg.AppMsg;
 import com.frostwire.android.R;
+import com.frostwire.android.gui.services.Engine;
 import com.frostwire.util.Ref;
 
 import java.lang.ref.WeakReference;
@@ -55,15 +55,6 @@ public final class ApolloUtils {
     public static boolean isLandscape(final Context context) {
         final int orientation = context.getResources().getConfiguration().orientation;
         return orientation == Configuration.ORIENTATION_LANDSCAPE;
-    }
-
-    /**
-     * Execute an {@link AsyncTask} on a thread pool
-     *
-     * @param task Task to execute
-     */
-    public static void execute(AsyncTask<Void, ?, ?> task) {
-        task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     /**
