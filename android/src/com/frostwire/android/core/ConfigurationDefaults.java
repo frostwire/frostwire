5dec2ac467871714523d0b7882187f3d5334d011
gubatron <gubatron@gmail.com>
2020-07-07 14:20:57 -0600

    [android] Make MagnetDL and One3337x available search engines like the rest

--- android/src/com/frostwire/android/core/ConfigurationDefaults.java
+++ android/src/com/frostwire/android/core/ConfigurationDefaults.java
@@ -112,6 +112,8 @@ final class ConfigurationDefaults {
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_YIFY, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_TORRENTSFM, true);
         defaultValues.put(Constants.PREF_KEY_SEARCH_USE_TORRENTZ2, true);
+        defaultValues.put(Constants.PREF_KEY_SEARCH_USE_MAGNETDL, true);
+        defaultValues.put(Constants.PREF_KEY_SEARCH_USE_ONE337X, true);
 
         defaultValues.put(Constants.PREF_KEY_NETWORK_ENABLE_DHT, true);
         defaultValues.put(Constants.PREF_KEY_NETWORK_USE_WIFI_ONLY, false);
