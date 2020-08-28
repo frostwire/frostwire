6a01ba94331d176da6857c994349909697fea72f
gubatron <gubatron@gmail.com>
2020-08-27 19:30:28 -0600

    [tests] com.frostwire.tests.EztvSearchPerformerTest to JUnit

--- desktop/src/test/java/com/frostwire/tests/EztvSearchPerformerTest.java
+++ desktop/src/test/java/com/frostwire/tests/EztvSearchPerformerTest.java
@@ -22,13 +22,18 @@ import com.frostwire.regex.Pattern;
 import com.frostwire.search.SearchMatcher;
 import com.frostwire.search.eztv.EztvSearchResult;
 import com.frostwire.util.HttpClientFactory;
+import com.frostwire.util.StringUtils;
 import com.frostwire.util.http.HttpClient;
+import org.junit.jupiter.api.Test;
 
 import static com.frostwire.search.eztv.EztvSearchPerformer.SEARCH_RESULTS_REGEX;
 import static com.frostwire.search.eztv.EztvSearchPerformer.TORRENT_DETAILS_PAGE_REGEX;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public final class EztvSearchPerformerTest {
-    public static void main(String[] args) throws Throwable {
+    @Test
+    public void eztvSearchTest() {
         String TEST_SEARCH_TERM = "foo";
         HttpClient httpClient = HttpClientFactory.newInstance();
         String fileStr;
@@ -37,17 +42,20 @@ public final class EztvSearchPerformerTest {
         } catch (Throwable t) {
             t.printStackTrace();
             System.out.println("Aborting test.");
+            fail(t.getMessage());
             return;
         }
         Pattern searchResultsDetailURLPattern = Pattern.compile(SEARCH_RESULTS_REGEX);
         Pattern detailPagePattern = Pattern.compile(TORRENT_DETAILS_PAGE_REGEX);
         Matcher searchResultsMatcher = searchResultsDetailURLPattern.matcher(fileStr);
         int found = 0;
-        while (searchResultsMatcher.find()) {
+        while (searchResultsMatcher.find() && found < 5) {
             found++;
             System.out.println("\nfound " + found);
-            System.out.println("result_url: [" + searchResultsMatcher.group(1) + "]");
-            String detailUrl = "https://eztv.re" + searchResultsMatcher.group(1);
+            String result_url = searchResultsMatcher.group(1);
+            assertTrue(!StringUtils.isNullOrEmpty(result_url), "result_url was null or empty");
+            System.out.println("result_url: [" + result_url + "]");
+            String detailUrl = "https://eztv.re" + result_url;
             System.out.println("Fetching details from " + detailUrl + " ....");
             long start = System.currentTimeMillis();
             String detailPage;
@@ -55,9 +63,10 @@ public final class EztvSearchPerformerTest {
                 detailPage = httpClient.get(detailUrl, 5000);
             } catch (Throwable t) {
                 detailPage = null;
+                fail(t.getMessage());
             }
             if (detailPage == null) {
-                System.out.println("Error fetching from " + detailUrl);
+                System.err.println("Error fetching from " + detailUrl);
                 continue;
             }
             long downloadTime = System.currentTimeMillis() - start;
@@ -65,22 +74,38 @@ public final class EztvSearchPerformerTest {
             SearchMatcher sm = new SearchMatcher(detailPagePattern.matcher(detailPage));
             if (sm.find()) {
                 System.out.println("magneturl: [" + sm.group("magneturl") + "]");
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("magneturl")), "magneturl was null or empty");
                 System.out.println("torrenturl: [" + sm.group("torrenturl") + "]");
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("torrenturl")), "torrenturl was null or empty");
                 System.out.println("seeds: [" + sm.group("seeds") + "]");
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("seeds")), "seeds was null or empty");
                 System.out.println("displayname: [" + sm.group("displayname") + "]");
                 System.out.println("displayname2: [" + sm.group("displayname2") + "]");
                 System.out.println("displaynamefallback: [" + sm.group("displaynamefallback") + "]");
+
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("displayname")) ||
+                                !StringUtils.isNullOrEmpty(sm.group("displayname2")) ||
+                                !StringUtils.isNullOrEmpty(sm.group("displaynamefallback"))
+                        , "displayname && displaynam2 && displaynamefallback were all null or empty");
+
                 System.out.println("infohash: [" + sm.group("infohash") + "]");
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("infohash")), "infohash was null or empty");
                 System.out.println("filesize: [" + sm.group("filesize") + "]");
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("filesize")), "filesize was null or empty");
                 System.out.println("creationtime: [" + sm.group("creationtime") + "]");
+                assertTrue(!StringUtils.isNullOrEmpty(sm.group("creationtime")), "creationtime was null or empty");
                 EztvSearchResult sr = new EztvSearchResult(detailUrl, sm);
                 System.out.println(sr);
             } else {
-                System.out.println("Detail page search matcher failed, check TORRENT_DETAILS_PAGE_REGEX");
+                fail("Detail page search matcher failed, check TORRENT_DETAILS_PAGE_REGEX");
             }
             System.out.println("===");
             System.out.println("Sleeping 5 seconds...");
-            Thread.sleep(5000);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         System.out.println("-done-");
     }
