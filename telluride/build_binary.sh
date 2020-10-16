a144946b51198080284d97369e7cf7d9f41abf03
frostwire <contact@frostwire.com>
2020-10-16 13:39:37 -0600

    [telluride] builds binary in ubuntu, cleans up everything but the built binary

--- telluride/build_binary.sh
+++ telluride/build_binary.sh
@@ -1,2 +1,45 @@
 #!/bin/bash
-pyinstaller --onefile telluride.py
+PYINSTALLER_CMD="pyinstaller"
+
+if [ $(uname -a | grep -c Ubuntu) == 1 ]
+then
+    PYINSTALLER_CMD="${HOME}/.local/bin/pyinstaller"
+    echo "PYINSTALLER_CMD=${PYINSTALLER_CMD}"
+fi
+
+cleanup() {
+if [ -d build ]
+then
+  rm -fr build
+fi
+
+if [ -d dist ]
+then
+  rm -fr dist
+fi
+
+if [ -d __pycache__ ]
+then
+  rm -fr __pycache__
+fi
+if [ -f telluride.spec ]
+then
+  rm telluride.spec
+fi
+}
+
+cleanup
+${PYINSTALLER_CMD} --onefile telluride.py
+
+if [ -f dist/telluride ]
+then
+  if [ $(uname -a | grep -c Ubuntu) == 1 ]
+  then
+    mv dist/telluride telluride_linux
+  elif [ $(uname -a | grep -c Darwin) == 1 ]
+  then
+    mv dist/telluride telluride_macos
+  fi
+  cleanup
+  ls -lth
+fi
