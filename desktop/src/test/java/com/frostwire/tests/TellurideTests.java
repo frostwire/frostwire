8696fab914e6381fa6c9af19fbea9fb7a6539964
gubatron <gubatron@gmail.com>
2020-10-24 16:26:20 -0600

    [desktop] TellurideTests tests for failure. sync fixes.
     - We test for a failed URL
     - We were triggering the latch before failing
     - fail() can't be executed on child thread, we add errors to a List on main thread, if list isn't empty we fail with first error after latch is released

--- desktop/src/test/java/com/frostwire/tests/TellurideTests.java
+++ desktop/src/test/java/com/frostwire/tests/TellurideTests.java
@@ -23,6 +23,8 @@ import com.frostwire.util.OSUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -46,8 +48,78 @@ public class TellurideTests {
     static boolean progressWasReported = false;
 
     @Test
+    public void testConnectionError() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        List<String> failedTests = new ArrayList<>();
+        TellurideListener tellurideListener = new TellurideListener() {
+            @Override
+            public void onProgress(float completionPercentage, float fileSize, String fileSizeUnits, float downloadSpeed, String downloadSpeedUnits, String ETA) {
+                System.out.println("[TellurideTests][testConnectionError] onProgress(completionPercentage=" +
+                        completionPercentage + ", fileSize=" +
+                        fileSize + ", fileSizeUnits=" +
+                        fileSizeUnits + ", downloadSpeed=" +
+                        downloadSpeed + ", downloadSpeedUnits=" +
+                        downloadSpeedUnits + ", ETA=" + ETA + ")");
+                progressWasReported = true;
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                failedTests.add("[TellurideTests][testConnectionError] onError(errorMessage=" + errorMessage + ")");
+            }
+
+            @Override
+            public void onFinished(int exitCode) {
+                if (exitCode != 0) {
+                    failedTests.add("[TellurideTests][testConnectionError] onFinished(exitCode=" + exitCode + ")");
+                }
+                if (!progressWasReported) {
+                    failedTests.add("[TellurideTests][testConnectionError] Failed, did not receive onProgress call. onFinished(exitCode=" + exitCode + ")");
+                }
+                latch.countDown();
+            }
+
+            @Override
+            public void onDestination(String outputFilename) {
+                System.out.println("[TellurideTests][testConnectionError] onDestination(outputFilename=" + outputFilename + ")");
+            }
+
+            @Override
+            public boolean aborted() {
+                return false;
+            }
+
+            @Override
+            public void onMeta(String json) {
+                System.out.println("[TellurideTests][testConnectionError] GOT JSON!");
+                System.out.println(json);
+            }
+
+            @Override
+            public int hashCode() {
+                return 1;
+            }
+        };
+
+        TellurideLauncher.launch(new File("/Users/gubatron/workspace.frostwire/frostwire/telluride/telluride" + executableSuffix),
+                "https://www.youtube.com/watch?v=fail",
+                new File("/Users/gubatron/FrostWire/Torrent Data"),
+                false,
+                false,
+                tellurideListener);
+
+        System.out.println("[TellurideTests][testConnectionError] waiting...");
+        latch.await();
+        if (failedTests.size() > 0) {
+            fail(failedTests.get(0));
+        }
+        System.out.println("[TellurideTests][testConnectionError] finished.");
+    }
+
+    @Test
     public void testDownload() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
+        List<String> failedTests = new ArrayList<>();
         TellurideListener tellurideListener = new TellurideListener() {
             @Override
             public void onProgress(float completionPercentage, float fileSize, String fileSizeUnits, float downloadSpeed, String downloadSpeedUnits, String ETA) {
@@ -62,18 +134,18 @@ public class TellurideTests {
 
             @Override
             public void onError(String errorMessage) {
-                fail("[TellurideTests][testDownload] onError(errorMessage=" + errorMessage + ")");
+                failedTests.add("[TellurideTests][testDownload] onError(errorMessage=" + errorMessage + ")");
             }
 
             @Override
             public void onFinished(int exitCode) {
-                latch.countDown();
                 if (exitCode != 0) {
-                    fail("[TellurideTests][testDownload] onFinished(exitCode=" + exitCode + ")");
+                    failedTests.add("[TellurideTests][testDownload] onFinished(exitCode=" + exitCode + ")");
                 }
                 if (!progressWasReported) {
-                    fail("[TellurideTests][testDownload] Failed, did not receive onProgress call. onFinished(exitCode=" + exitCode + ")");
+                    failedTests.add("[TellurideTests][testDownload] Failed, did not receive onProgress call. onFinished(exitCode=" + exitCode + ")");
                 }
+                latch.countDown();
             }
 
             @Override
@@ -107,12 +179,16 @@ public class TellurideTests {
 
         System.out.println("[TellurideTests][testDownload] waiting...");
         latch.await();
+        if (failedTests.size() > 0) {
+            fail(failedTests.get(0));
+        }
         System.out.println("[TellurideTests][testDownload] finished.");
     }
 
     @Test
     public void testMetaOnly() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
+        List<String> failedTests = new ArrayList<>();
         TellurideListener tellurideListener = new TellurideListener() {
             @Override
             public void onProgress(float completionPercentage, float fileSize, String fileSizeUnits, float downloadSpeed, String downloadSpeedUnits, String ETA) {
@@ -120,15 +196,15 @@ public class TellurideTests {
 
             @Override
             public void onError(String errorMessage) {
-                fail("[TellurideTests][testMetaOnly] onError(errorMessage=" + errorMessage + ")");
+                failedTests.add("[TellurideTests][testMetaOnly] onError(errorMessage=" + errorMessage + ")");
             }
 
             @Override
             public void onFinished(int exitCode) {
-                latch.countDown();
                 if (exitCode != 0) {
-                    fail("[TellurideTests][testMetaOnly] onFinished(exitCode=" + exitCode + ")");
+                    failedTests.add("[TellurideTests][testMetaOnly] onFinished(exitCode=" + exitCode + ")");
                 }
+                latch.countDown();
             }
 
             @Override
@@ -161,6 +237,9 @@ public class TellurideTests {
 
         System.out.println("[TellurideTests][testMetaOnly] waiting...");
         latch.await();
+        if (failedTests.size() > 0) {
+            fail(failedTests.get(0));
+        }
         System.out.println("[TellurideTests][testMetaOnly] finished.");
     }
 }
