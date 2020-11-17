409789461d9ace4f4f5ab2db0e2f1236f9ad0caa
gubatron <gubatron@gmail.com>
2020-11-17 16:35:31 -0700

    [desktop] typo fix

--- desktop/src/com/limegroup/gnutella/gui/search/SourceRenderer.java
+++ desktop/src/com/limegroup/gnutella/gui/search/SourceRenderer.java
@@ -47,7 +47,7 @@ public class SourceRenderer extends DefaultTableBevelledCellRenderer implements 
             sourceIcons.put("limetorrents", GUIMediator.getThemeImage("limetorrent")); // Thank you trollmad3 for the icon
             sourceIcons.put("eztv", GUIMediator.getThemeImage("eztv_off"));
             sourceIcons.put("zooqle", GUIMediator.getThemeImage("zooqle_source"));
-            sourceIcons.put("magnetdl", GuiMediator.getThemeImage("magnetdl_source"));
+            sourceIcons.put("magnetdl", GUIMediator.getThemeImage("magnetdl_source"));
             sourceIcons.put("default", GUIMediator.getThemeImage("seeding_small_source"));
             sourceIcons.put("1337x", GUIMediator.getThemeImage("1337_source")); // Thank you AholicKnight for the icon.
             //TODO icon for torrentz2
