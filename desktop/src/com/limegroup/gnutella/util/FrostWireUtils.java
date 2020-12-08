a04d3762fdf82adbd84d4e6e89cf30eec556c1f7
gubatron <gubatron@gmail.com>
2020-12-08 01:09:13 -0700

    [desktop] 6.8.9 build 300

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,11 +38,11 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.8.8";
+    private static final String FROSTWIRE_VERSION = "6.8.9";
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 299;
+    private static final int BUILD_NUMBER = 300;
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
     /**
