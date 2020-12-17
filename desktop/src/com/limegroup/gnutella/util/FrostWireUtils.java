a7298c11b543e18d008025ce9d46bd8da1669ac8
gubatron <gubatron@gmail.com>
2020-12-16 18:24:02 -0700

    [desktop] 6.8.10 version bump - changelog

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,7 +38,7 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.8.9";
+    private static final String FROSTWIRE_VERSION = "6.8.10";
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
