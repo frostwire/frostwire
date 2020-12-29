3538c5659d655e15404daa548b74ea8eb60293f9
gubatron <gubatron@gmail.com>
2020-12-29 14:35:55 -0700

    [common] New FilenameUtils.sanitizeFilename and cleanWindowsIllegalFileNameChars

--- common/src/main/java/org/apache/commons/io/FilenameUtils.java
+++ common/src/main/java/org/apache/commons/io/FilenameUtils.java
@@ -16,8 +16,11 @@
  */
 package org.apache.commons.io;
 
+import com.frostwire.util.StringUtils;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Stack;
 
@@ -331,6 +334,31 @@ public class FilenameUtils {
     public static String normalizeNoEndSeparator(String filename, boolean unixSeparator) {
         char separator = unixSeparator ? UNIX_SEPARATOR : WINDOWS_SEPARATOR;
         return doNormalize(filename, separator, false);
+    }
+
+    final static int[] illegalChars = {34, 60, 62, 124, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 58, 42, 63, 92, 47};
+    static {
+        Arrays.sort(illegalChars);
+    }
+    public static String cleanWindowsIllegalFileNameChars(String badFileName) {
+        StringBuilder cleanName = new StringBuilder();
+        for (int i = 0; i < badFileName.length(); i++) {
+            int c = (int)badFileName.charAt(i);
+            if (Arrays.binarySearch(illegalChars, c) < 0) {
+                cleanName.append((char)c);
+            }
+        }
+        return cleanName.toString();
+    }
+
+    /** Makes sure the given file name is cleaned up of any illegal characters,
+     * double spaces and unicode characters that might prevent libtorrent
+     * from creating torrents or http downloads from being written to disk
+     * (especially in Windows)*/
+    public static String sanitizeFilename(String filename) {
+        String withoutInvalidChars = cleanWindowsIllegalFileNameChars(filename);
+        String withoutUnicodeChars = StringUtils.removeUnicodeCharacters(withoutInvalidChars);
+        return StringUtils.removeDoubleSpaces(withoutUnicodeChars);
     }
 
     /**
