9cb86afee7ad47c18f3e07f5f2caecb92335936a
gubatron <gubatron@gmail.com>
2020-12-28 21:17:24 -0700

    [telluride] macos signature and notarization

--- telluride/sign.sh
+++ telluride/sign.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-codesign --verbose=4 -s KET68JTS3L telluride_macos
+codesign --verbose=4 -s KET68JTS3L --entitlements Entitlements.plist -f telluride_macos
 codesign -vvv telluride_macos
+
+# This tool must be symlinked from our private repo given it includes credentials
+./notarizeMacOsApp.sh telluride_macos com.frostwire.Telluride
