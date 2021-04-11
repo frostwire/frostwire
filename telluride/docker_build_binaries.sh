78a0e63f1cbfa395012554fc2b07a964f9d82ceb
frostwire <contact@frostwire.com>
2021-04-11 10:35:22 -0600

    [telluride] docker_build_binaries.sh
    We now have deterministic telluride builds for android, yay

---
+++ telluride/docker_build_binaries.sh
@@ -0,0 +1,12 @@
+# One Step Build (It will be cached if nothing changed)
+./docker_build_image.sh
+
+# Mounts this repo's folder as a volume in the container's /telluride-ubuntu folder
+# Then executes the build scripts
+docker \
+ run \
+ --cpus 4 \
+ -v "$PWD/../telluride:/telluride-ubuntu" \
+ -it telluride-ubuntu \
+ /bin/bash -c "cd telluride-ubuntu && ./configure_update.sh && ./build.sh"
+
