b5d6e1105ef9c8e5aab181baea5ab98965d4fc9a
gubatron <gubatron@gmail.com>
2021-10-03 08:20:10 -0600

    [common] SimpleDateFormat missing locale warning

--- common/src/main/java/com/frostwire/search/yify/YifySearchResult.java
+++ common/src/main/java/com/frostwire/search/yify/YifySearchResult.java
@@ -1,12 +1,12 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2017, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2021, FrostWire(R). All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,10 +20,12 @@ package com.frostwire.search.yify;
 import com.frostwire.search.PerformersHelper;
 import com.frostwire.search.SearchMatcher;
 import com.frostwire.search.torrent.AbstractTorrentSearchResult;
+
 import org.apache.commons.io.FilenameUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -88,7 +90,7 @@ public final class YifySearchResult extends AbstractTorrentSearchResult {
 
     private static long buildCreationTime(String str) {
         try {
-            return new SimpleDateFormat("M/d/y").parse(str).getTime();
+            return new SimpleDateFormat("M/d/y", Locale.US).parse(str).getTime();
         } catch (Throwable e) {
             // not that important
             return System.currentTimeMillis();
