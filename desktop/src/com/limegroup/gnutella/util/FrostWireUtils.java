e1cf384c246b1ab0db0d62c90144a25762c5426e
frostwire <contact@frostwire.com>
2021-11-20 09:51:55 -0700

    [desktop] 6.9.6 bump

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,7 +38,7 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.9.5";
+    private static final String FROSTWIRE_VERSION = "6.9.6";
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
