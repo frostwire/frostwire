b481d300217aa1bf19ba71773a043c3baaea866a
gubatron <gubatron@gmail.com>
2022-04-25 13:05:32 -0600

    [desktop] 6.9.8 build 312 bump

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,11 +38,11 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.9.7";
+    private static final String FROSTWIRE_VERSION = "6.9.8";
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 311;
+    private static final int BUILD_NUMBER = 312;
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
     /**
