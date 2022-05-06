cf62cbfa59aeeea7f3840d27fa6969192ad74e6e
gubatron <gubatron@gmail.com>
2022-05-05 18:20:38 -0600

    [android] ImageViewerActivity, ImageViewerFragment, My Files Fragment removal cleanup

--- android/src/com/frostwire/android/gui/adapters/menu/OpenMenuAction.java
+++ android/src/com/frostwire/android/gui/adapters/menu/OpenMenuAction.java
@@ -1,12 +1,12 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2020, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2022, FrostWire(R). All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,16 +18,13 @@
 package com.frostwire.android.gui.adapters.menu;
 
 import android.content.Context;
-import android.content.Intent;
 
 import com.andrew.apollo.utils.MusicUtils;
 import com.frostwire.android.R;
 import com.frostwire.android.core.Constants;
 import com.frostwire.android.core.FWFileDescriptor;
-import com.frostwire.android.gui.activities.ImageViewerActivity;
 import com.frostwire.android.gui.activities.MainActivity;
 import com.frostwire.android.gui.dialogs.HandpickedTorrentDownloadDialogOnFetch;
-import com.frostwire.android.gui.fragments.ImageViewerFragment;
 import com.frostwire.android.gui.transfers.TransferManager;
 import com.frostwire.android.gui.util.UIUtils;
 import com.frostwire.android.gui.views.MenuAction;
@@ -82,12 +79,7 @@ public class OpenMenuAction extends MenuAction {
 
     @Override
     public void onClick(Context context) {
-        if (fileType == Constants.FILE_TYPE_PICTURES && fd != null) {
-            Intent intent = new Intent(context, ImageViewerActivity.class);
-            intent.putExtra(ImageViewerFragment.EXTRA_FILE_DESCRIPTOR_BUNDLE, fd.toBundle());
-            intent.putExtra(ImageViewerFragment.EXTRA_ADAPTER_FILE_OFFSET, position);
-            context.startActivity(intent);
-        } else if (fileType == Constants.FILE_TYPE_RINGTONES) {
+        if (fileType == Constants.FILE_TYPE_RINGTONES) {
             if (MusicUtils.isPlaying()) {
                 MusicUtils.playPauseOrResume();
             }
