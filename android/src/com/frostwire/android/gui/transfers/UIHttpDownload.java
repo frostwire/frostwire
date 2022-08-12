468244bf80b37a77ac28adc07aea97ee080b08ab
gubatron <gubatron@gmail.com>
2022-08-12 15:30:45 -0600

    [android] license updates

--- android/src/com/frostwire/android/gui/transfers/UIHttpDownload.java
+++ android/src/com/frostwire/android/gui/transfers/UIHttpDownload.java
@@ -1,25 +1,26 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2016, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2022, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.frostwire.android.gui.transfers;
 
+import android.content.Context;
 import android.media.MediaScannerConnection;
 
+import com.frostwire.android.gui.MainApplication;
 import com.frostwire.android.gui.services.Engine;
 import com.frostwire.android.util.SystemUtils;
 import com.frostwire.frostclick.Slide;
@@ -68,11 +69,21 @@ public class UIHttpDownload extends HttpDownload {
     protected void moveAndComplete(File src, File dst) {
         super.moveAndComplete(src, dst);
         if (SystemUtils.hasAndroid11OrNewer()) {
-            //postToHandler(SystemUtils.HandlerThreadName.DOWNLOADER, () -> Librarian.mediaStoreSaveToDownloads(dst, dst, true));
-            MediaScannerConnection.scanFile(Engine.instance().getApplication(),
-                    new String[]{dst.getAbsolutePath()},
-                    new String[]{MimeDetector.getMimeType(FilenameUtils.getExtension(dst.getName()))},
-                    (path, uri) -> LOG.info("UIHttpDownload::moveAndComplete() -> mediaScan complete on " + dst));
+            Context context;
+            try {
+                context = Engine.instance().getApplication();
+                if (context == null) {
+                    context = MainApplication.context();
+                }
+            } catch (Throwable ignored) {
+                context = MainApplication.context();
+            }
+            if (context != null) {
+                MediaScannerConnection.scanFile(context,
+                        new String[]{dst.getAbsolutePath()},
+                        new String[]{MimeDetector.getMimeType(FilenameUtils.getExtension(dst.getName()))},
+                        (path, uri) -> LOG.info("UIHttpDownload::moveAndComplete() -> mediaScan complete on " + dst));
+            }
         }
     }
 
