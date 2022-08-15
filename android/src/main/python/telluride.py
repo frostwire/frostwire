bbe0b36c97614ef7c3bb00ac818d207544c7d3bb
gubatron <gubatron@gmail.com>
2022-08-15 17:22:11 -0600

    [android] telluride.query_page returns None on error

--- android/src/main/python/telluride.py
+++ android/src/main/python/telluride.py
@@ -5,6 +5,7 @@ import json
 def query_video(page_url):
     '''
     query_video: queries metadata for the video in page_url using youtube_dl
+    Returns 'None' on error
     '''
     ydl_opts = {'nocheckcertificate' : True,
                 'quiet': True,
@@ -12,10 +13,13 @@ def query_video(page_url):
                 'format': 'bestaudio/best'
                }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(urllib.parse.unquote(page_url), download=False)
-        return json.dumps(info_dict, indent=4)
+        try:
+            info_dict = ydl.extract_info(urllib.parse.unquote(page_url), download=False)
+            return json.dumps(info_dict, indent=4)
+        except:
+            return None
 
 if __name__ == '__main__':
     r = query_video("https://www.youtube.com/watch?v=ye2CLllRO8I")
     print(r)
-    print(len(r))
+
