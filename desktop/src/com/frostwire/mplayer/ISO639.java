5bffd9f1b094d7413f00f9d11865495766b7b1c9
gubatron <gubatron@gmail.com>
2022-10-18 15:18:13 -0600

    [desktop] JDK 19 Locale deprecation updates

--- desktop/src/com/frostwire/mplayer/ISO639.java
+++ desktop/src/com/frostwire/mplayer/ISO639.java
@@ -1,3 +1,20 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2022, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.frostwire.mplayer;
 
 import java.util.HashMap;
@@ -208,6 +225,6 @@ class ISO639 {
         if (iso3 == null) return null;
         String iso2 = ISO639.getCode(iso3);
         if (iso2 == null) return null;
-        return new Locale(iso2);
+        return new Locale.Builder().setLanguage(iso2).build();
     }
 }
