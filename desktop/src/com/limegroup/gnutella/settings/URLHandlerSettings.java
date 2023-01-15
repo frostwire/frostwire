d7188974ca1380b28a8d11d8f517b295952b89a4
gubatron <gubatron@gmail.com>
2023-01-15 07:57:21 -0700

    [desktop] default video player in linux is mplayer

--- desktop/src/com/limegroup/gnutella/settings/URLHandlerSettings.java
+++ desktop/src/com/limegroup/gnutella/settings/URLHandlerSettings.java
@@ -1,3 +1,20 @@
+/*
+ * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
+ * Copyright (c) 2011-2023, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.limegroup.gnutella.settings;
 
 import org.limewire.setting.StringSetting;
@@ -15,7 +32,7 @@ public final class URLHandlerSettings extends LimeProps {
      * Setting for which movie player to use
      */
     public static final StringSetting VIDEO_PLAYER =
-            FACTORY.createStringSetting("VIDEO_PLAYER", "vlc $URL$");
+            FACTORY.createStringSetting("VIDEO_PLAYER", "mplayer $URL$");
     /**
      * Setting for which image viewer to use
      */
@@ -25,7 +42,7 @@ public final class URLHandlerSettings extends LimeProps {
      * Setting for which audio player to use
      */
     public static final StringSetting AUDIO_PLAYER =
-            FACTORY.createStringSetting("AUDIO_PLAYER", "vlc $URL$");
+            FACTORY.createStringSetting("AUDIO_PLAYER", "mplayer $URL$");
 
     private URLHandlerSettings() {
     }
