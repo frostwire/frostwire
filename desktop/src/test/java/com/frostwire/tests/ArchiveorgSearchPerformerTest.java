1269b09ddc20ea6a6288adb3d36b5a95eb507628
gubatron <gubatron@gmail.com>
2023-02-28 20:54:10 -0700

    [common] license header, less verbose test

--- desktop/src/test/java/com/frostwire/tests/ArchiveorgSearchPerformerTest.java
+++ desktop/src/test/java/com/frostwire/tests/ArchiveorgSearchPerformerTest.java
@@ -1,3 +1,20 @@
+/*
+ * Created by Angel Leon (@gubatron)
+ * Copyright (c) 2011-2023, FrostWire(R). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.frostwire.tests;
 
 import com.frostwire.search.*;
@@ -32,13 +49,8 @@ public class ArchiveorgSearchPerformerTest {
                     }
                     ArchiveorgCrawledSearchResult sr = (ArchiveorgCrawledSearchResult) r;
                     searchResults.add(sr);
-                    LOG.info("[ArchiveorgSearchPerformerTest] display: " + sr.getDisplayName());
-                    LOG.info("[ArchiveorgSearchPerformerTest] details: " + sr.getDetailsUrl());
                     LOG.info("[ArchiveorgSearchPerformerTest] source: " + sr.getSource());
                     LOG.info("[ArchiveorgSearchPerformerTest] size: " + sr.getSize());
-                    LOG.info("[ArchiveorgSearchPerformerTest] thumbnail: " + sr.getThumbnailUrl());
-                    LOG.info("[ArchiveorgSearchPerformerTest] filename: " + sr.getFilename());
-                    LOG.info("[ArchiveorgSearchPerformerTest] download url: " + sr.getDownloadUrl());
                     LOG.info("[ArchiveorgSearchPerformerTest] ---------------------");
                 }
             }
