8274632218b3d77f2e56218e0aa226df29cf6d32
gubatron <gubatron@gmail.com>
2023-03-03 11:19:59 -0700

    [desktop] FrostWire 6.10.0 is ready

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,7 +38,7 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.9.10";
+    private static final String FROSTWIRE_VERSION = "6.10.0";
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
