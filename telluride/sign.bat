e938e3391f8030b6d4a23215cc750d1ac885a538
gubatron <gubatron@gmail.com>
2023-10-18 05:34:43 -0600

    [telluride] sign.bat simplification
    It no longer requires environment variables specifying cert location and password as we now are able to sign using a hardware key which interacts with the windows certificate manager

--- telluride/sign.bat
+++ telluride/sign.bat
@@ -1,20 +1,3 @@
 @echo OFF
-:: These environment variables are set in windows10_gitbash_profile, cert pass must be updated regularly.
-IF NOT DEFINED CERT_PATH (
-  @echo ON
-  echo The environment variable CERT_PATH is not set. Aborting signing.
-  exit /b 1
-)
-
-IF NOT DEFINED CERT_PASS (
-  @echo ON
-  echo The environment variable CERT_PASS is not set. Aborting signing.
-  exit /b 1
-)
-
 REM @echo ON
-echo Signing using the following environment variable values:
-echo CERT_PATH=%CERT_PATH%
-echo You will have to enter this CERT_PASS=%CERT_PASS% in the SafeNet eToken window dialog to sign properly
-
-"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /f %CERT_PATH% /p %CERT_PASS% /n "FrostWire" /fd sha256 /tr http://timestamp.digicert.com /v "telluride.exe"
+"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /v /sm /s MY /n "FrostWire LLC" /tr http://timestamp.digicert.com /v "telluride.exe"
