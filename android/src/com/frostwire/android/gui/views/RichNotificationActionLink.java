20d7e8052e0de3d510680e1e2dcf0e384b466d25
Marcelina Knitter <marcelina.knitter@gmail.com>
2024-10-04 15:12:48 -0600

    renaming colors
    Since app blue color becomes dark grey in night theme of the app I did some renaming so its easier to choose colors.

--- android/src/com/frostwire/android/gui/views/RichNotificationActionLink.java
+++ android/src/com/frostwire/android/gui/views/RichNotificationActionLink.java
@@ -64,7 +64,7 @@ public class RichNotificationActionLink {
             SpannableString text = new SpannableString(getText());
             text.setSpan(new UnderlineSpan(), 0, text.length(), 0);
             tv.setText(text);
-            tv.setTextColor(contextReference.get().getResources().getColor(R.color.basic_blue));
+            tv.setTextColor(contextReference.get().getResources().getColor(R.color.basic_color));
             tv.setOnClickListener(getClickAdapter());
             tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15.0f);
             result = tv;
