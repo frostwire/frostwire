4603ddf149fe4e3d4e8be2274fa418aa2ce9910f
Avery King <gperson@disroot.org>
2025-09-04 08:17:07 -0700

    [common] Fix Archive.org search provider URL
    It turns out there was an invalid argument to the 'indent' parameter in
    the URL. Simply removing this URL makes the search provider work again,
    so searching Archive.org from FrostWire now works again.

--- common/src/main/java/com/frostwire/search/archiveorg/ArchiveorgSearchPerformer.java
+++ common/src/main/java/com/frostwire/search/archiveorg/ArchiveorgSearchPerformer.java
@@ -47,7 +47,7 @@ public class ArchiveorgSearchPerformer extends CrawlPagedWebSearchPerformer<Arch
                 + getDomainName()
                 + "/advancedsearch.php?q="
                 + encodedKeywords
-                + "&fl[]=avg_rating&fl[]=call_number&fl[]=collection&fl[]=contributor&fl[]=coverage&fl[]=creator&fl[]=date&fl[]=description&fl[]=downloads&fl[]=foldoutcount&fl[]=format&fl[]=headerImage&fl[]=identifier&fl[]=imagecount&fl[]=language&fl[]=licenseurl&fl[]=mediatype&fl[]=month&fl[]=num_reviews&fl[]=oai_updatedate&fl[]=publicdate&fl[]=publisher&fl[]=rights&fl[]=scanningcentre&fl[]=source&fl[]=title&fl[]=type&fl[]=volume&fl[]=week&fl[]=year&rows=50&page=1&indent=yes&output=json";
+                + "&fl[]=avg_rating&fl[]=call_number&fl[]=collection&fl[]=contributor&fl[]=coverage&fl[]=creator&fl[]=date&fl[]=description&fl[]=downloads&fl[]=foldoutcount&fl[]=format&fl[]=headerImage&fl[]=identifier&fl[]=imagecount&fl[]=language&fl[]=licenseurl&fl[]=mediatype&fl[]=month&fl[]=num_reviews&fl[]=oai_updatedate&fl[]=publicdate&fl[]=publisher&fl[]=rights&fl[]=scanningcentre&fl[]=source&fl[]=title&fl[]=type&fl[]=volume&fl[]=week&fl[]=year&rows=50&page=1&output=json";
         //sort[]=downloads+desc&sort[]=createdate+desc
         //sort[]=avg_rating+desc&
     }
