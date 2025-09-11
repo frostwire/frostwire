2bb56c8905581a0baa307a28601a69718cbd2a81
gubatron <gubatron@gmail.com>
2025-09-11 14:25:15 -0600

    [desktop] SwingUtilities.isEventDispatchThread to EventQueue.isDispatchThread and BackgroundExecutorService renamed to BackgroundQueueExecutorService
    The name is meant to denote that this executor service runs only a single thread and it queues tasks in a blocking list.

--- desktop/src/com/limegroup/gnutella/gui/search/AbstractUISearchResult.java
+++ desktop/src/com/limegroup/gnutella/gui/search/AbstractUISearchResult.java
@@ -1,19 +1,18 @@
 /*
  * Created by Angel Leon (@gubatron), Alden Torres (aldenml)
- * Copyright (c) 2011-2018, FrostWire(R). All rights reserved.
+ * Copyright (c) 2011-2025, FrostWire(R). All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.limegroup.gnutella.gui.search;
@@ -27,11 +26,11 @@ import com.frostwire.search.telluride.TellurideSearchResult;
 import com.frostwire.search.yt.YTSearchResult;
 import com.limegroup.gnutella.MediaType;
 import com.limegroup.gnutella.gui.GUIMediator;
-import com.limegroup.gnutella.gui.util.BackgroundExecutorService;
+import com.limegroup.gnutella.gui.util.BackgroundQueuedExecutorService;
 import com.limegroup.gnutella.settings.SearchSettings;
 import org.apache.commons.io.FilenameUtils;
 
-import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author gubatron
@@ -114,16 +113,12 @@ public abstract class AbstractUISearchResult implements UISearchResult {
     @Override
     public void play() {
         // this gets invoked when clicking on a search result play preview button.
-        if (sr instanceof StreamableSearchResult) {
-            StreamableSearchResult ssr = (StreamableSearchResult) sr;
-
-            String streamUrl;
-            if (SwingUtilities.isEventDispatchThread() && sr instanceof SoundcloudSearchResult) {
-                SoundcloudSearchResult scsr = (SoundcloudSearchResult) sr;
+        if (sr instanceof StreamableSearchResult ssr) {
+            if (EventQueue.isDispatchThread() && sr instanceof SoundcloudSearchResult scsr) {
                 if (scsr.fetchedDownloadUrl()) {
                     playStream(ssr.getStreamUrl());
                 } else {
-                    BackgroundExecutorService.schedule(() -> {
+                    BackgroundQueuedExecutorService.schedule(() -> {
                         String url = ssr.getStreamUrl();
                         GUIMediator.safeInvokeLater(() -> playStream(url));
                     });
