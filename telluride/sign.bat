8c8d1a1614ca7481bf6878f9a6a2aafc930c8200
gubatron <gubatron@gmail.com>
2025-09-12 19:59:36 -0600

    Removed telluride, will re-add as submodule from github.com/frostwire/telluride

--- telluride/sign.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-REM @echo ON
-"C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe" sign /v /sm /s MY /n "FrostWire LLC" /tr http://timestamp.digicert.com /fd SHA256 /td SHA256 "telluride.exe"
