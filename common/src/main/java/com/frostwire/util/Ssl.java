a4b4f4e6b08d4b42eeca9b4c08904c0d92039ebb
Avery King <gperson@disroot.org>
2025-09-13 20:09:24 -0700

    Fix GloTorrents search
    This was a similar to the Archive.org search provider: just update the
    URL! Now we have a working GloTorrents search provider again!

--- common/src/main/java/com/frostwire/util/Ssl.java
+++ common/src/main/java/com/frostwire/util/Ssl.java
@@ -114,7 +114,7 @@ public final class Ssl {
                 "google.com",
                 "googleapis.com",
                 "gstatic.com",
-                "gtdb.to",
+                "gtso.cc",
                 "idope.se",
                 "igcdn.com",
                 "igsonar.com",
