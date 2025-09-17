5fd8d15988723ff2ac26692114e76790ee181277
gubatron <gubatron@gmail.com>
2025-09-17 14:55:03 -0600

    [desktop] 7.0.0 version bump and changelog

--- desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
+++ desktop/src/com/limegroup/gnutella/util/FrostWireUtils.java
@@ -38,11 +38,11 @@ public final class FrostWireUtils {
     /**
      * Constant for the current version of FrostWire.
      */
-    private static final String FROSTWIRE_VERSION = "6.14.0";
+    private static final String FROSTWIRE_VERSION = "7.0.0";
     /**
      * Build number for the current version, gets reset to 1 on every version bump
      */
-    private static final int BUILD_NUMBER = 326;
+    private static final int BUILD_NUMBER = 327;
     private static final boolean IS_RUNNING_FROM_SOURCE = new File("README.md").exists();
 
     /**
