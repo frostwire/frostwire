d2ef79481a921cb54e65f82b433a9446578b34e0
gubatron <gubatron@gmail.com>
2026-04-06 13:02:18 -0600

    AboutFragment: Html.fromHtml(String) → fromHtml with FROM_HTML_MODE_LEGACY on API 24+

--- android/src/main/java/com/frostwire/android/gui/fragments/AboutFragment.java
+++ android/src/main/java/com/frostwire/android/gui/fragments/AboutFragment.java
@@ -108,7 +108,10 @@ public final class AboutFragment extends AbstractFragment {
         setupClickUrl(contactUs, Constants.CONTACT_US_URL);
 
         TextView content = findView(rootView, R.id.fragment_about_content);
-        content.setText(Html.fromHtml(getAboutText()));
+        String aboutHtml = getAboutText();
+        content.setText(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N
+                ? Html.fromHtml(aboutHtml, android.text.Html.FROM_HTML_MODE_LEGACY)
+                : android.text.Html.fromHtml(aboutHtml));
         content.setMovementMethod(LinkMovementMethod.getInstance());
 
         if (Constants.IS_GOOGLE_PLAY_DISTRIBUTION) {
