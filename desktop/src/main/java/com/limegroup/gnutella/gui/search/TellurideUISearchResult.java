bec1a0fcdcf3da9a3bfab435a18c840d563b30df
gubatron <gubatron@gmail.com>
2026-08-28 12:21:21 -0600

    [desktop] Forward Telluride HTTP headers into openHttp

--- desktop/src/main/java/com/limegroup/gnutella/gui/search/TellurideUISearchResult.java
+++ desktop/src/main/java/com/limegroup/gnutella/gui/search/TellurideUISearchResult.java
@@ -1,17 +1,17 @@
 /*
  *     Created by Angel Leon (@gubatron), Alden Torres (aldenml)
  *     Copyright (c) 2011-2026, FrostWire(R). All rights reserved.
- * 
+ *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -23,42 +23,61 @@ import com.frostwire.search.telluride.TellurideSearchResult;
 import com.limegroup.gnutella.gui.GUIMediator;
 import com.limegroup.gnutella.gui.I18n;
 import com.limegroup.gnutella.gui.util.PopupUtils;
-
 import javax.swing.*;
 
 public class TellurideUISearchResult extends AbstractUISearchResult {
 
-    private final boolean extractAudio;
+  private final boolean extractAudio;
 
-    public TellurideUISearchResult(TellurideSearchResult sr, SearchEngine se, String query, boolean extractAudio) {
-        super(sr, se, query);
-        this.extractAudio = extractAudio;
-    }
+  public TellurideUISearchResult(
+      TellurideSearchResult sr, SearchEngine se, String query, boolean extractAudio) {
+    super(sr, se, query);
+    this.extractAudio = extractAudio;
+  }
 
-    @Override
-    public void download(boolean partial) {
-        TellurideSearchResult sr = (TellurideSearchResult) getSearchResult();
-        GUIMediator.instance().showTransfers(TransfersTab.FilterMode.ALL);
-        GUIMediator.instance().openHttp(sr.getDownloadUrl(), sr.getDisplayName(), sr.getFilename(), sr.getSize(), extractAudio);
-        showSearchResultWebPage(false);
-    }
+  @Override
+  public void download(boolean partial) {
+    TellurideSearchResult sr = (TellurideSearchResult) getSearchResult();
+    GUIMediator.instance().showTransfers(TransfersTab.FilterMode.ALL);
+    GUIMediator.instance()
+        .openHttp(
+            sr.getDownloadUrl(),
+            sr.getDisplayName(),
+            sr.getFilename(),
+            sr.getSize(),
+            extractAudio,
+            sr.getHttpHeaders());
+    showSearchResultWebPage(false);
+  }
 
-    @Override
-    public JPopupMenu createMenu(JPopupMenu popupMenu, SearchResultDataLine[] lines, SearchResultMediator rp) {
-        TellurideSearchResult sr = (TellurideSearchResult) getSearchResult();
-        PopupUtils.addMenuItem(I18n.tr("Copy Download URL"), e -> GUIMediator.setClipboardContent(sr.getDownloadUrl()), popupMenu, true, 0);
-        PopupUtils.addMenuItem(SearchMediator.DOWNLOAD_STRING, e -> download(false), popupMenu, lines.length > 0, 2);
-        PopupUtils.addMenuItem(SearchMediator.TELLURIDE_DETAILS_STRING + " " + sr.getSource(), e -> showSearchResultWebPage(true), popupMenu, lines.length == 1, 3);
-        return popupMenu;
-    }
+  @Override
+  public JPopupMenu createMenu(
+      JPopupMenu popupMenu, SearchResultDataLine[] lines, SearchResultMediator rp) {
+    TellurideSearchResult sr = (TellurideSearchResult) getSearchResult();
+    PopupUtils.addMenuItem(
+        I18n.tr("Copy Download URL"),
+        e -> GUIMediator.setClipboardContent(sr.getDownloadUrl()),
+        popupMenu,
+        true,
+        0);
+    PopupUtils.addMenuItem(
+        SearchMediator.DOWNLOAD_STRING, e -> download(false), popupMenu, lines.length > 0, 2);
+    PopupUtils.addMenuItem(
+        SearchMediator.TELLURIDE_DETAILS_STRING + " " + sr.getSource(),
+        e -> showSearchResultWebPage(true),
+        popupMenu,
+        lines.length == 1,
+        3);
+    return popupMenu;
+  }
 
-    @Override
-    public String getHash() {
-        return null;
-    }
+  @Override
+  public String getHash() {
+    return null;
+  }
 
-    @Override
-    public int getSeeds() {
-        return 250;
-    }
+  @Override
+  public int getSeeds() {
+    return 250;
+  }
 }
