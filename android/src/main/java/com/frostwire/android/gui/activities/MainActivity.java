94f24b54d0a4f881853f1646e721cbad994b88f8
gubatron <gubatron@gmail.com>
2026-09-01 17:35:23 -0600

    fix(android): restore transfers while VPN guard blocks

--- android/src/main/java/com/frostwire/android/gui/activities/MainActivity.java
+++ android/src/main/java/com/frostwire/android/gui/activities/MainActivity.java
@@ -495,13 +495,11 @@ public class MainActivity extends AbstractActivity
       if (ConfigurationManager.instance()
               .getBoolean(Constants.PREF_KEY_NETWORK_BITTORRENT_ON_VPN_ONLY)
           && !NetworkManager.instance().isTunnelUp()) {
-        TransferManager.instance().pauseTorrents();
         UIUtils.showDismissableMessage(
             findView(R.id.activity_main_parent_layout), R.string.cannot_start_engine_without_vpn);
-      } else {
-        firstTime = false;
-        Engine.instance().startServices();
       }
+      firstTime = false;
+      Engine.instance().startServices();
     }
     if (Engine.instance().wasShutdown()) {
       Engine.instance().startServices();
