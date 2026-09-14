c85c4580adf027cad7a687704edd0435d6c96e94
Angel Leon <gubatron@gmail.com>
2026-09-14 12:01:10 -0600

    [desktop] Give IconAndNameHolderImpl value equality
    Recreated per model read; compares shared icon instances by identity and names by value, with null-safe name handling.
    
    opencode/muse-spark-1.3-contributor-free

--- desktop/src/main/java/com/limegroup/gnutella/gui/tables/IconAndNameHolderImpl.java
+++ desktop/src/main/java/com/limegroup/gnutella/gui/tables/IconAndNameHolderImpl.java
@@ -1,6 +1,7 @@
 package com.limegroup.gnutella.gui.tables;
 
 import javax.swing.*;
+import java.util.Objects;
 
 /**
  * Default implementation of IconAndNameHolder.
@@ -20,6 +21,26 @@ public final class IconAndNameHolderImpl implements IconAndNameHolder,
 
     public int compareTo(IconAndNameHolderImpl o) {
         return AbstractTableMediator.compare(_name, o._name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof IconAndNameHolderImpl)) {
+            return false;
+        }
+        IconAndNameHolderImpl other = (IconAndNameHolderImpl) o;
+        // Icons have no value equality; shared icon instances compare by identity.
+        return _icon == other._icon && Objects.equals(_name, other._name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = System.identityHashCode(_icon);
+        result = 31 * result + Objects.hashCode(_name);
+        return result;
     }
 
     public Icon getIcon() {
